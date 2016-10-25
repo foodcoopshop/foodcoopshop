d5f10508386a4268731719a3af1f3ed43e88a6e7
Mario Rothauer <marothauer@gmail.com>
2016-10-25 09:53:04 +0200

    bugfix table prefix

--- Model/Product.php
+++ Model/Product.php
@@ -434,7 +434,7 @@ class Product extends AppModel
         
         // avoid Integrity constraint violation: 1062 Duplicate entry '64-232-1-0' for key 'product_sqlstock'
         // with custom sql
-        $this->query('INSERT INTO ".$this->tablePrefix."stock_available (id_product, id_product_attribute, id_shop, quantity) VALUES(' . $productId . ', ' . $productAttributeId . ', 1, ' . $defaultQuantity . ')');
+        $this->query('INSERT INTO '.$this->tablePrefix.'stock_available (id_product, id_product_attribute, id_shop, quantity) VALUES(' . $productId . ', ' . $productAttributeId . ', 1, ' . $defaultQuantity . ')');
         
         $this->StockAvailable->updateQuantityForMainProduct($productId);
     }
