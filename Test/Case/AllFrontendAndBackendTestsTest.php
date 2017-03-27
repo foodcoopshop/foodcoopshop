b03513200185b88b54c0c62944f87d716d166821
Mario Rothauer <marothauer@gmail.com>
2017-03-27 20:36:24 +0200

    all tests can be run by using new suite

---
+++ Test/Case/AllFrontendAndBackendTestsTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * AllTestsTest
+ *
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 1.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, http://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+class AllTestsTest extends PHPUnit_Framework_TestSuite
+{
+
+    /**
+     * suite method, defines tests for this suite.
+     *
+     * @return void
+     */
+    public static function suite()
+    {
+        $suite = new CakeTestSuite('All Tests');
+        $suite->addTestDirectoryRecursive(TESTS . 'Case');
+        $suite->addTestDirectoryRecursive(APP.DS.'Plugin'.DS.'Admin'.DS.'Test'.DS.'Case');
+        return $suite;
+    }
+}
