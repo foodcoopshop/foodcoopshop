4ef5d6e3078b53bb8f67a6d3309ee254cf06538f
Mario Rothauer <marothauer@gmail.com>
2017-04-02 20:39:22 +0200

    fix: db version in test db

--- Test/test_files/Config/sql/test-db-data.sql
+++ Test/test_files/Config/sql/test-db-data.sql
@@ -693,8 +693,8 @@ INSERT INTO `fcs_configuration` (`id_configuration`, `id_shop_group`, `id_shop`,
 (558, NULL, NULL, 1, 'FCS_APP_EMAIL', 'E-Mail-Adresse der Foodcoop<br /><div class="small"></div>', 'demo-foodcoop@maillinator.com', 'text', 7, '2017-01-12 00:00:00', '2017-01-12 00:00:00'),
 (559, NULL, NULL, 1, 'FCS_PLATFORM_OWNER', 'Betreiber der Plattform<br /><div class="small">Für Datenschutzerklärung und Nutzungsbedingungen, bitte auch Adresse angeben. Kann leer gelassen werden, wenn die Foodcoop selbst die Plattform betreibt.</div>', '', 'textarea', 8, '2017-01-12 00:00:00', '2017-01-12 00:00:00'),
 (560, NULL, NULL, 1, 'FCS_SHOP_ORDER_DEFAULT_STATE', 'Bestellstatus für Sofort-Bestellungen', '1', 'dropdown', 75, '2017-01-12 00:00:00', '2017-01-12 00:00:00'),
-(561, NULL, NULL, 1, 'FCS_DB_VERSION', 'Version der Datenbank-Struktur', '0', 'readonly', 0, '2017-03-13 00:00:00', '2017-03-13 00:00:00'),
-(562, NULL, NULL, 0, 'FCS_DB_UPDATE', 'Version des letzten versuchten Datenbank-Updates', '0', 'readonly', 0, '2017-03-13 00:00:00', '2017-03-13 00:00:00');
+(561, NULL, NULL, 1, 'FCS_DB_VERSION', 'Version der Datenbank-Struktur', '1', 'readonly', 0, '2017-03-13 00:00:00', '2017-03-13 00:00:00'),
+(562, NULL, NULL, 0, 'FCS_DB_UPDATE', 'Version des letzten versuchten Datenbank-Updates', '1', 'readonly', 0, '2017-03-13 00:00:00', '2017-03-13 00:00:00');
 
 --
 -- TRUNCATE Tabelle vor dem Einfügen `fcs_customer`
