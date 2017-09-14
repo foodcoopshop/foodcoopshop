581351ca7494697b2c4bb57a98c4f400fa6afb98
Mario Rothauer <marothauer@gmail.com>
2017-09-14 18:26:23 +0200

    db based network plugin config

--- Config/sql/migrations/100.sql
+++ Config/sql/migrations/100.sql
@@ -14,3 +14,5 @@ CREATE TABLE `fcs_sync_products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `fcs_manufacturer` ADD `enabled_sync_domains` VARCHAR(50) DEFAULT NULL AFTER `send_ordered_product_quantity_changed_notification`;
+
+INSERT INTO `fcs_configuration` (`id_configuration`, `id_shop_group`, `id_shop`, `active`, `name`, `text`, `value`, `type`, `position`, `date_add`, `date_upd`) VALUES ('0', NULL, NULL, '1', 'FCS_NETWORK_PLUGIN_ENABLED', 'Netzwerk-Plugin aktiviert?<br /><div class=\"small\"><a href="https://foodcoopshop.github.io/de/netzwerk-plugin" target="_blank">Infos zum Netzwerk-Plugin</a></div>', '0', 'readonly', '50', '2017-09-14 00:00:00', '2017-09-14 00:00:00');
--- Test/test_files/Config/sql/test-db-data.sql
+++ Test/test_files/Config/sql/test-db-data.sql
@@ -654,7 +654,8 @@ INSERT INTO `fcs_configuration` VALUES
 (563,NULL,NULL,1,'FCS_EMAIL_LOG_ENABLED','Sollen alle ausgehenden E-Mails in der Datenbank gespeichert werden?<br /><div class=\"small\">Für Debugging gedacht.</div>','1','readonly',30,'2017-07-05 00:00:00','2017-07-05 00:00:00'),
 (564,NULL,NULL,1,'FCS_ORDER_COMMENT_ENABLED','Kommentarfeld bei Bestell-Abschluss anzeigen?<br /><div class=\"small\">Wird im Admin-Bereich unter \"Bestellungen\" angezeigt.</div>','1','boolean',13,'2017-07-09 00:00:00','2017-07-09 00:00:00'),
 (565,NULL,NULL,1,'FCS_USE_VARIABLE_MEMBER_FEE','Variablen Mitgliedsbeitrag verwenden?<br /><div class=\"small\">Den variablen Mitgliedsbeitrag bei den Hersteller-Rechnungen abziehen? Die Produkt-Preise müssen entsprechend höher eingegeben werden.</div>','0','readonly',40,'2017-08-02 00:00:00','2017-08-02 00:00:00'),
-(566,NULL,NULL,1,'FCS_DEFAULT_VARIABLE_MEMBER_FEE_PERCENTAGE','Standardwert für variablen Mitgliedsbeitrag<br /><div class=\"small\">Der Prozentsatz kann in den Hersteller-Einstellungen auch individuell angepasst werden.</div>','0','readonly',50,'2017-08-02 00:00:00','2017-08-02 00:00:00');
+(566,NULL,NULL,1,'FCS_DEFAULT_VARIABLE_MEMBER_FEE_PERCENTAGE','Standardwert für variablen Mitgliedsbeitrag<br /><div class=\"small\">Der Prozentsatz kann in den Hersteller-Einstellungen auch individuell angepasst werden.</div>','0','readonly',50,'2017-08-02 00:00:00','2017-08-02 00:00:00'),
+(567,NULL,NULL,1,'FCS_NETWORK_PLUGIN_ENABLED','Netzwerk-Plugin aktiviert?<br /><div class=\"small\"><a href=\"https://foodcoopshop.github.io/de/netzwerk-plugin\" target=\"_blank\">Infos zum Netzwerk-Plugin</a></div>','1','readonly',50,'2017-09-14 00:00:00','2017-09-14 00:00:00');
 /*!40000 ALTER TABLE `fcs_configuration` ENABLE KEYS */;
 
 /*!40000 ALTER TABLE `fcs_customer` DISABLE KEYS */;
