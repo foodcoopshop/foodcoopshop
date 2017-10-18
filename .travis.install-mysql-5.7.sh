15e52e5f71f74f1afa6bafa75ce95699a3554d5c
Mario Rothauer <marothauer@gmail.com>
2017-10-18 16:09:07 +0200

    update apt version

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,6 +1,7 @@
+# @see https://github.com/codyzu/jasql/blob/master/.travis.install-mysql-5.7.sh
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
-sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
+sudo dpkg --install mysql-apt-config_0.8.8-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o --allow-unauthenticated Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade --force
