c3d8ee9e1ebba378a085071587d3875571adb51f
Mario Rothauer <marothauer@gmail.com>
2017-10-18 16:47:36 +0200

    mysql 5.7 fix

--- Model/Product.php
+++ Model/Product.php
@@ -699,7 +699,7 @@ class Product extends AppModel
 
         // DISTINCT: attributes cause duplicate entries
         $fields = array(
-            'DISTINCT Product.id_product, Product.id_product, Product.active, Product.id_manufacturer, Product.id_tax, ProductLang.name'
+            'DISTINCT Product.id_product, Product.id_product, Product.active, Product.id_manufacturer, Product.id_tax, ProductLang.name, ImageShop.id_image'
         );
 
         $contain = array(
