852fce947f29bc09e1f1525f454be320c24d598f
Mario Rothauer <marothauer@gmail.com>
2017-10-18 16:17:39 +0200

    my

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -2,9 +2,9 @@
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
 sudo dpkg --install mysql-apt-config_0.8.8-1_all.deb
-sudo apt-get update -q
-sudo apt-get sudo apt-get install --allow-unauthenticated mysql-server
+sudo apt-get update
+sudo apt-get install mysql-server
 sudo mysql_secure_installation
-sudo mysql_upgrade --force
+sudo mysql_install_db
 service mysql status
-service mysql start
+service mysql restart
