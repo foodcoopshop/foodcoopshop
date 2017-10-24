4ac31c8282b1794f4d51bb406a45878cd0309107
Mario Rothauer <marothauer@gmail.com>
2017-10-24 16:36:46 +0200

    wording

--- Test/Case/Controller/PagesControllerTest.php
+++ Test/Case/Controller/PagesControllerTest.php
@@ -39,7 +39,7 @@ class PagesControllerTest extends AppCakeTestCase
             $this->Slug->getBlogList(),
             $this->Slug->getCategoryDetail(16, 'Fleischprodukte'),
             $this->Slug->getProductDetail(339, 'Kartoffel'),
-            $this->Slug->getBlogPostDetail(2, 'Demo Blog'),
+            $this->Slug->getBlogPostDetail(2, 'Demo Blog Artikel'),
             $this->Slug->getNewPasswordRequest(),
             $this->Slug->getPageDetail(9, 'Impressum'),
             $this->Slug->getLogin(),
