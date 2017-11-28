47865426e4941b4a28f68c3329ad59a1d1d68d6a
Mario Rothauer <marothauer@gmail.com>
2017-11-28 22:17:58 +0100

    unit tests for getOrderPeriodFirstDay

---
+++ Test/Case/View/Helper/MyTimeHelperTest.php
@@ -0,0 +1,81 @@
+<?php
+
+App::uses('AppCakeTestCase', 'Test');
+App::uses('View', 'View');
+App::uses('MyTimeHelper', 'View/Helper');
+
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 1.5.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, http://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+class MyTimeHelperTest extends AppCakeTestCase
+{
+
+    public function setUp()
+    {
+        $this->MyTimeHelper = new MyTimeHelper(new View());
+    }
+
+    public function testGetOrderPeriodFirstDayTuesdayFriday()
+    {
+        Configure::write('app.deliveryDayDelta', 3);
+        Configure::write('app.sendOrderListsWeekday', 2);
+
+        $this->assertGetOrderPeriodFirstDay('27.11.2017', '21.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('28.11.2017', '21.11.2017'); // tuesday
+        $this->assertGetOrderPeriodFirstDay('29.11.2017', '21.11.2017'); // wednesday
+        $this->assertGetOrderPeriodFirstDay('30.11.2017', '21.11.2017'); // thursday
+        $this->assertGetOrderPeriodFirstDay('01.12.2017', '21.11.2017'); // friday
+        $this->assertGetOrderPeriodFirstDay('02.12.2017', '28.11.2017'); // saturday
+        $this->assertGetOrderPeriodFirstDay('03.12.2017', '28.11.2017'); // sunday
+        $this->assertGetOrderPeriodFirstDay('04.12.2017', '28.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('05.12.2017', '28.11.2017'); // tuesday
+    }
+
+    public function testGetOrderPeriodFirstDayWednesdayFriday()
+    {
+        Configure::write('app.deliveryDayDelta', 2);
+        Configure::write('app.sendOrderListsWeekday', 3);
+
+        $this->assertGetOrderPeriodFirstDay('27.11.2017', '22.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('28.11.2017', '22.11.2017'); // tuesday
+        $this->assertGetOrderPeriodFirstDay('29.11.2017', '22.11.2017'); // wednesday
+        $this->assertGetOrderPeriodFirstDay('30.11.2017', '22.11.2017'); // thursday
+        $this->assertGetOrderPeriodFirstDay('01.12.2017', '22.11.2017'); // friday
+        $this->assertGetOrderPeriodFirstDay('02.12.2017', '29.11.2017'); // saturday
+        $this->assertGetOrderPeriodFirstDay('03.12.2017', '29.11.2017'); // sunday
+        $this->assertGetOrderPeriodFirstDay('04.12.2017', '29.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('05.12.2017', '29.11.2017'); // tuesday
+    }
+
+    public function testGetOrderPeriodFirstDayMondayTuesday()
+    {
+        Configure::write('app.deliveryDayDelta', 1);
+        Configure::write('app.sendOrderListsWeekday', 1);
+
+        $this->assertGetOrderPeriodFirstDay('27.11.2017', '20.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('28.11.2017', '20.11.2017'); // tuesday
+        $this->assertGetOrderPeriodFirstDay('29.11.2017', '27.11.2017'); // wednesday
+        $this->assertGetOrderPeriodFirstDay('30.11.2017', '27.11.2017'); // thursday
+        $this->assertGetOrderPeriodFirstDay('01.12.2017', '27.11.2017'); // friday
+        $this->assertGetOrderPeriodFirstDay('02.12.2017', '27.11.2017'); // saturday
+        $this->assertGetOrderPeriodFirstDay('03.12.2017', '27.11.2017'); // sunday
+        $this->assertGetOrderPeriodFirstDay('04.12.2017', '27.11.2017'); // monday
+        $this->assertGetOrderPeriodFirstDay('05.12.2017', '27.11.2017'); // tuesday
+    }
+
+    private function assertGetOrderPeriodFirstDay($currentDay, $expected)
+    {
+        $result = $this->MyTimeHelper->getOrderPeriodFirstDay(strtotime($currentDay));
+        $this->assertEquals($expected, $result);
+    }
+}
