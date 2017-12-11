bf057707567eb30017eed5a0f05b84b6369e1a77
Mario Rothauer <marothauer@gmail.com>
2017-12-11 11:00:56 +0100

    rename dev host tld to localhost

--- Config/travis/custom.config.php
+++ Config/travis/custom.config.php
@@ -48,7 +48,7 @@ Configure::write('app.additionalOrderStatusChangeInfo', '');
 /**
  * your host's name, eg. http://www.yourfoodcoop.com
  */
-Configure::write('app.cakeServerName', 'http://www.foodcoopshop.dev');
+Configure::write('app.cakeServerName', 'http://www.foodcoopshop.localhost');
 
 /**
  * array of email adresses that receive notifications after new member registrations
