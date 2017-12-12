43887b018d1e1e7352c94344090d273842dabae9
Mario Rothauer <marothauer@gmail.com>
2017-12-12 10:54:28 +0100

    use .test as dev tld

--- Config/travis/custom.config.php
+++ Config/travis/custom.config.php
@@ -48,7 +48,7 @@ Configure::write('app.additionalOrderStatusChangeInfo', '');
 /**
  * your host's name, eg. http://www.yourfoodcoop.com
  */
-Configure::write('app.cakeServerName', 'http://www.foodcoopshop.localhost');
+Configure::write('app.cakeServerName', 'http://www.foodcoopshop.test');
 
 /**
  * array of email adresses that receive notifications after new member registrations
