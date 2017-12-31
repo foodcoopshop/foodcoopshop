1697a2ddda71a0e5dbcd05b68b84316564da3ffe
Mario Rothauer <marothauer@gmail.com>
2017-12-31 01:41:31 +0100

    fix: valid customers were removed [ci skip]

--- Config/sql/migrations/018.sql
+++ Config/sql/migrations/018.sql
@@ -1,5 +1,4 @@
 DELETE FROM fcs_address WHERE deleted = 1;
-DELETE FROM fcs_address WHERE active = 0;
 DELETE FROM fcs_address WHERE email IS NULL;
 ALTER TABLE `fcs_address` CHANGE `other` `comment` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
