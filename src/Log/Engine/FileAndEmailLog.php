26c362c1e0bb37c92fe4ac86ee2e7b36a6ebc0b8
Mario Rothauer <marothauer@gmail.com>
2018-03-13 09:21:33 +0100

    do not log MissingRouteException

--- src/Log/Engine/FileAndEmailLog.php
+++ src/Log/Engine/FileAndEmailLog.php
@@ -40,7 +40,7 @@ class FileAndEmailLog extends FileLog
     private function sendEmailWithErrorInformation($message)
     {
 
-        $ignoredExceptionsRegex = '/(MissingController|MissingAction|RecordNotFound)Exception/';
+        $ignoredExceptionsRegex = '/(MissingController|MissingAction|RecordNotFound|MissingRoute)Exception/';
         if (preg_match($ignoredExceptionsRegex, $message)) {
             return false;
         }
