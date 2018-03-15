6743a572421cd1e230c9f18c03fa168396b6588b
Mario Rothauer <marothauer@gmail.com>
2018-03-15 09:47:55 +0100

    category ids fix

--- plugins/Admin/src/Controller/CategoriesController.php
+++ plugins/Admin/src/Controller/CategoriesController.php
@@ -37,6 +37,9 @@ class CategoriesController extends AdminAppController
             ['validate' => false]
         );
         $this->set('title_for_layout', 'Kategorie erstellen');
+        
+        $this->set('disabledSelectCategoryIds', []);
+        
         $this->_processForm($category, false);
 
         if (empty($this->request->getData())) {
