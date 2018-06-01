3330a5fb358d160a08ef78e3db716cdf7894d57b
Mario Rothauer <marothauer@gmail.com>
2018-06-01 12:09:25 +0200

    unused imports

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -31,7 +31,6 @@ require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
 use Cake\Cache\Cache;
 use Cake\Console\ConsoleErrorHandler;
-use Cake\Core\App;
 use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Core\Plugin;
@@ -42,7 +41,6 @@ use Cake\Http\ServerRequest;
 use Cake\Log\Log;
 use Cake\Mailer\Email;
 use Cake\ORM\TableRegistry;
-use Cake\Utility\Inflector;
 use Cake\Utility\Security;
 
 /*
