fe7200e12532dcee5a099452282158b05cb4f3a4
Mario Rothauer <marothauer@gmail.com>
2018-06-05 16:54:25 +0200

    set locales as variable

--- devtools/update-pot-and-po-files.sh
+++ devtools/update-pot-and-po-files.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+source $(dirname $0)/locales.sh
+
 #get and merge translations for main app
 bin/cake i18n extract --ignore-model-validation --output src\\Locale --paths src,config --overwrite --extract-core no --merge yes --no-location --exclude plugins
-msgmerge src/Locale/de_DE/default.po src/Locale/default.pot --output-file=src/Locale/de_DE/default.po
-msgmerge src/Locale/en_US/default.po src/Locale/default.pot --output-file=src/Locale/en_US/default.po
+for locale in "${LOCALES[@]}"
+do
+    msgmerge src/Locale/$locale/default.po src/Locale/default.pot --output-file=src/Locale/$locale/default.po
+done
 
 #get and merge translations for admin plugin
 bin/cake i18n extract --plugin Admin --ignore-model-validation --overwrite --extract-core no --merge yes --no-location
-msgmerge plugins/Admin/src/Locale/de_DE/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/de_DE/admin.po
-msgmerge plugins/Admin/src/Locale/en_US/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/en_US/admin.po
+for locale in "${LOCALES[@]}"
+do
+    msgmerge plugins/Admin/src/Locale/$locale/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/$locale/admin.po
+done
 
 #todo: get and merge translations for network plugin
