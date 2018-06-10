3a094831debe1d640e44c70cfeb63ef5ac893e25
Mario Rothauer <marothauer@gmail.com>
2018-06-10 22:54:15 +0200

    width msgmerge

--- devtools/update-pot-and-po-files.sh
+++ devtools/update-pot-and-po-files.sh
@@ -6,19 +6,19 @@ source $(dirname $0)/locales.sh
 bin/cake i18n extract --ignore-model-validation --output src\\Locale --paths src,config --overwrite --extract-core no --merge yes --no-location --exclude plugins
 for locale in "${LOCALES[@]}"
 do
-    msgmerge src/Locale/$locale/default.po src/Locale/default.pot --output-file=src/Locale/$locale/default.po --no-wrap
+    msgmerge src/Locale/$locale/default.po src/Locale/default.pot --output-file=src/Locale/$locale/default.po --width=86
 done
 
 #get and merge translations for admin plugin
 bin/cake i18n extract --plugin Admin --ignore-model-validation --overwrite --extract-core no --merge yes --no-location
 for locale in "${LOCALES[@]}"
 do
-    msgmerge plugins/Admin/src/Locale/$locale/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/$locale/admin.po --no-wrap
+    msgmerge plugins/Admin/src/Locale/$locale/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/$locale/admin.po --width=86
 done
 
 #get and merge translations for network plugin
 bin/cake i18n extract --plugin Network --ignore-model-validation --overwrite --extract-core no --merge yes --no-location
 for locale in "${LOCALES[@]}"
 do
-    msgmerge plugins/Network/src/Locale/$locale/network.po plugins/Network/src/Locale/default.pot --output-file=plugins/Network/src/Locale/$locale/network.po --no-wrap
+    msgmerge plugins/Network/src/Locale/$locale/network.po plugins/Network/src/Locale/default.pot --output-file=plugins/Network/src/Locale/$locale/network.po --width=86
 done
