3e2e48328e668d15777bc3550f29b52c480e1255
Mario Rothauer <marothauer@gmail.com>
2018-06-10 13:20:08 +0200

    add field locale to fcs_configuration

--- config/Migrations/20180601101119_LocaleConfig.php
+++ config/Migrations/20180601101119_LocaleConfig.php
@@ -6,6 +6,8 @@ class LocaleConfig extends AbstractMigration
     public function change()
     {
         $this->execute("
+            ALTER TABLE `fcs_configuration` ADD `locale` VARCHAR(5) NULL DEFAULT NULL AFTER `position`;
+            UPDATE fcs_configuration SET locale = 'de_DE';
         ");
     }
 }
