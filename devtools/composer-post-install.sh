5ccd2d6ae0675939932f609719270265539fc994
Mario Rothauer <marothauer@gmail.com>
2018-07-03 11:25:06 +0200

    debug

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -2,7 +2,7 @@
 #bash devtools/setup-git.sh
 #rm -Rf vendor/studio-42/elfinder/.git
 #mkdir -p webroot/js/elfinder
-#ls -l vendor
+ls -l vendor
 ls -l vendor/studio-42
 #cp -Rp vendor/studio-42/elfinder/* webroot/js/elfinder
 #rm -Rf vendor/studio-42
