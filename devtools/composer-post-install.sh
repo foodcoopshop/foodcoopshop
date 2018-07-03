0d3a2cc5ac9912ca5a9945258377710c6b8ceac7
Mario Rothauer <marothauer@gmail.com>
2018-07-03 10:48:45 +0200

    line breaks

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 bash devtools/setup-git.sh
-
 rm -Rf vendor/studio-42/elfinder/.git
 mkdir -p webroot/node_modules/elfinder
 cp -Rp vendor/studio-42/elfinder/* webroot/node_modules/elfinder
