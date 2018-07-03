139d7abd135d4aa4bf6357f454de5896873eb4d2
Mario Rothauer <marothauer@gmail.com>
2018-07-03 11:13:59 +0200

    debug

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -2,5 +2,7 @@
 bash devtools/setup-git.sh
 rm -Rf vendor/studio-42/elfinder/.git
 mkdir -p webroot/js/elfinder
+ls -l vendor/studio-42/elfinder
+ls -l webroot/js/elfinder
 cp -Rp vendor/studio-42/elfinder/* webroot/js/elfinder
 #rm -Rf vendor/studio-42
