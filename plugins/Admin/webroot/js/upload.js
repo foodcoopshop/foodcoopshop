33d575019812eecb2c7db872bbbe0b3ea191bde9
Mario Rothauer <marothauer@gmail.com>
2018-11-22 10:47:41 +0100

    avoid code duplication

--- plugins/Admin/webroot/js/upload.js
+++ plugins/Admin/webroot/js/upload.js
@@ -140,63 +140,21 @@ foodcoopshop.Upload = {
 
                 autoUpload: false,
 
-                // This function is called when a file is added to the queue;
-                // either via the browse button, or via drag/drop:
                 add: function (e, data) {
-
-                    var tpl = $('<li class="working"><p></p><input type="text" value="0" data-width="48" data-height="48"'+
-                        ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /></li><div class="sc"></div>');
-
-                    // Append the file name and file size
-                    tpl.find('p').text(data.files[0].name);
-
-                    // Add the HTML to the UL element
-                    data.context = tpl.appendTo(ul);
-
-                    // Initialize the knob plugin
-                    tpl.find('input').knob();
-
-                    // Listen for clicks on the cancel icon
-                    tpl.find('span').click(function () {
-
-                        if (tpl.hasClass('working')) {
-                            jqXHR.abort();
-                        }
-
-                        tpl.fadeOut(function () {
-                            tpl.remove();
-                        });
-
-                    });
-
-                    // Automatically upload the file once it is added to the queue
-                    var jqXHR = data.submit();
+                    foodcoopshop.Upload.fileUploadAdd(e, data, ul);
                 },
 
-                progress: function (e, data) {
-
-                    // Calculate the completion percentage of the upload
-                    var progress = parseInt(data.loaded / data.total * 100, 10);
-
-                    // Update the hidden input field and trigger a change
-                    // so that the jQuery knob plugin knows to update the dial
-                    data.context.find('input').val(progress).change();
-
-                    if (progress == 100) {
-                        data.context.removeClass('working');
-                    }
-                },
-
+                progress: foodcoopshop.Upload.fileUploadProgress,
+                
                 done: function (e, data) {
 
                     fileUploadForm.find('ul li').remove();
                     fileUploadForm.find('a.uploadedFile').remove();
-                    fileUploadForm.find('.modify-icon').remove();
 
                     var result = JSON.parse(data.result);
                     if (result.status) {
                         var container = fileUploadForm.find('.drop');
-                        container.find('img').remove();
+                        container.find('a').remove();
                         container.prepend($('<a />').
                             attr('href', result.filename).
                             addClass('uploadedFile'));
@@ -221,7 +179,56 @@ foodcoopshop.Upload = {
             });
 
         });
-    },    
+    },
+
+    /**
+     * This function is called when a file is added to the queue;
+     * either via the browse button, or via drag/drop:
+     */
+    fileUploadAdd : function (e, data, ul) {
+
+        var tpl = $('<li class="working"><p></p><input type="text" value="0" data-width="48" data-height="48"'+
+            ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /></li><div class="sc"></div>');
+
+        // Append the file name and file size
+        tpl.find('p').text(data.files[0].name);
+
+        // Add the HTML to the UL element
+        data.context = tpl.appendTo(ul);
+
+        // Initialize the knob plugin
+        tpl.find('input').knob();
+
+        // Listen for clicks on the cancel icon
+        tpl.find('span').click(function () {
+
+            if (tpl.hasClass('working')) {
+                jqXHR.abort();
+            }
+
+            tpl.fadeOut(function () {
+                tpl.remove();
+            });
+
+        });
+
+        // Automatically upload the file once it is added to the queue
+        var jqXHR = data.submit();
+    },
+    
+    fileUploadProgress : function (e, data) {
+
+        // Calculate the completion percentage of the upload
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+
+        // Update the hidden input field and trigger a change
+        // so that the jQuery knob plugin knows to update the dial
+        data.context.find('input').val(progress).change();
+
+        if (progress == 100) {
+            data.context.removeClass('working');
+        }
+    },
 
     initUploadButtonImage : function (container) {
 
@@ -281,52 +288,11 @@ foodcoopshop.Upload = {
 
                 autoUpload: false,
 
-                // This function is called when a file is added to the queue;
-                // either via the browse button, or via drag/drop:
                 add: function (e, data) {
-
-                    var tpl = $('<li class="working"><p></p><input type="text" value="0" data-width="48" data-height="48"'+
-                        ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /></li><div class="sc"></div>');
-
-                    // Append the file name and file size
-                    tpl.find('p').text(data.files[0].name);
-
-                    // Add the HTML to the UL element
-                    data.context = tpl.appendTo(ul);
-
-                    // Initialize the knob plugin
-                    tpl.find('input').knob();
-
-                    // Listen for clicks on the cancel icon
-                    tpl.find('span').click(function () {
-
-                        if (tpl.hasClass('working')) {
-                            jqXHR.abort();
-                        }
-
-                        tpl.fadeOut(function () {
-                            tpl.remove();
-                        });
-
-                    });
-
-                    // Automatically upload the file once it is added to the queue
-                    var jqXHR = data.submit();
+                    foodcoopshop.Upload.fileUploadAdd(e, data, ul);
                 },
 
-                progress: function (e, data) {
-
-                    // Calculate the completion percentage of the upload
-                    var progress = parseInt(data.loaded / data.total * 100, 10);
-
-                    // Update the hidden input field and trigger a change
-                    // so that the jQuery knob plugin knows to update the dial
-                    data.context.find('input').val(progress).change();
-
-                    if (progress == 100) {
-                        data.context.removeClass('working');
-                    }
-                },
+                progress: foodcoopshop.Upload.fileUploadProgress,
 
                 done: function (e, data) {
 
