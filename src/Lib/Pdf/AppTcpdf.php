df9010464fd68e9d54a380e640f5eaba7426e9f1
Mario Rothauer <marothauer@gmail.com>
2019-01-14 00:46:51 +0100

    removed wrong change

--- src/Lib/Pdf/AppTcpdf.php
+++ src/Lib/Pdf/AppTcpdf.php
@@ -366,7 +366,7 @@ class AppTcpdf extends TCPDF
         $this->table .= '</tr>';
     }
 
-    public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskcache = false, $pdfas = false)
+    public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskcache = false, $pdfa = false)
     {
         parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
         mb_internal_encoding('UTF-8');
