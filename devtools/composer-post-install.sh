409adef47533a903b1d1d50b37ac98caaef787ca
Mario Rothauer <office@rothauer-it.com>
2019-02-04 13:44:40 +0100

    Feature/issue 317 shrink build (#318)
    * remove overhead data from npm libraries
    
    * remove overhead from composer dependencies
    
    * release shrinked

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -2,6 +2,13 @@
 
 bash devtools/setup-git.sh
 
+## remove overhead from tcpdf library
+rm -Rf vendor/tecnickcom/tcpdf/examples
+rm -Rf vendor/tecnickcom/tcpdf/fonts/ae_fonts_2.0
+rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.33
+rm -Rf vendor/tecnickcom/tcpdf/fonts/free*
+rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.34/status.txt
+
 rm -Rf vendor/studio-42/elfinder/.git
 mkdir -p webroot/js/elfinder
 cp -Rp vendor/studio-42/elfinder/* webroot/js/elfinder
--- src/Shell/NpmPostInstallShell.php
+++ src/Shell/NpmPostInstallShell.php
@@ -15,6 +15,7 @@
 
 namespace App\Shell;
 
+use Cake\Filesystem\File;
 use Cake\Filesystem\Folder;
 
 class NpmPostInstallShell extends AppShell
@@ -28,14 +29,60 @@ class NpmPostInstallShell extends AppShell
     public function main()
     {
         $this->vendorDir = WWW_ROOT . 'node_modules';
+        
+        $this->fontawesomePath = $this->vendorDir . DS . '@fortawesome' . DS . 'fontawesome-free' . DS;
+        $this->jqueryBackstretchPath = $this->vendorDir . DS . 'jquery-backstretch' . DS;
+        $this->jqueryUiPath = $this->vendorDir . DS . 'components-jqueryui' . DS;
+        $this->owlCarouselPath = $this->vendorDir . DS . 'owl.carousel' . DS;
+        $this->tooltipsterPath = $this->vendorDir . DS . 'tooltipster' . DS;
+        
+        $this->cleanOverheadFromDependencies();
         $this->copyAdaptedElfinderFiles();
         $this->copyJqueryUiImages();
         $this->copyFontawesomeFonts();
     }
+    
+    private function cleanOverheadFromDependencies()
+    {
+        
+        $folder = new Folder();
+        
+        $folder->delete($this->jqueryBackstretchPath . DS . 'examples');
+        $folder->delete($this->jqueryBackstretchPath . DS . 'test');
+        
+        $folder->delete($this->fontawesomePath . 'js');
+                
+        $file = new File($this->fontawesomePath . 'css' . DS . 'all.min.css');
+        $file->delete();
+        $file = new File($this->fontawesomePath . 'css' . DS . 'fontawesome.css');
+        $file->delete();
+        $file = new File($this->fontawesomePath . 'css' . DS . 'fontawesome.min.css');
+        $file->delete();
+        $file = new File($this->fontawesomePath . 'css' . DS . 'v4-shims.css');
+        $file->delete();
+        $file = new File($this->fontawesomePath . 'css' . DS . 'v4-shims.min.css');
+        $file->delete();
+        
+        $activeThemeFolder = 'smoothness';
+        $folder = new Folder($this->jqueryUiPath . 'themes' . DS . $activeThemeFolder);
+        $folder->copy($this->jqueryUiPath . 'theme-backup');
+        $folder->delete($this->jqueryUiPath . 'themes');
+        $folder = new Folder($this->jqueryUiPath . 'theme-backup');
+        $folder->move($this->jqueryUiPath . 'themes' . DS . $activeThemeFolder);
+        
+        $folder->delete($this->owlCarouselPath . 'docs');
+        $folder->delete($this->owlCarouselPath . 'docs_src');
+        $folder->delete($this->owlCarouselPath . 'src');
+        $folder->delete($this->owlCarouselPath . 'test');
+        
+        $folder->delete($this->tooltipsterPath . 'demo');
+        $folder->delete($this->tooltipsterPath . 'doc');
+        
+    }
 
     private function copyFontawesomeFonts()
     {
-        $folder = new Folder($this->vendorDir . DS . '@fortawesome' . DS . 'fontawesome-free' . DS . 'webfonts' . DS);
+        $folder = new Folder($this->fontawesomePath . 'webfonts' . DS);
         $folder->copy(WWW_ROOT . 'webfonts');
         $this->out('Fontawesome fonts copied.');
     }
@@ -46,7 +93,7 @@ class NpmPostInstallShell extends AppShell
      */
     private function copyJqueryUiImages()
     {
-        $folder = new Folder($this->vendorDir . DS . 'components-jqueryui' . DS. 'themes' . DS . 'smoothness' . DS . 'images' . DS);
+        $folder = new Folder($this->jqueryUiPath . 'themes' . DS . 'smoothness' . DS . 'images' . DS);
         $folder->copy(WWW_ROOT . 'cache' . DS . 'images');
         $this->out('JQueryUI images copied.');
     }
