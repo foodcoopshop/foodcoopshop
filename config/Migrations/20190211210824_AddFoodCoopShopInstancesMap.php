9e92c3f6db600b6b68601c6012d38adc549126b3
Mario Rothauer <marothauer@gmail.com>
2019-02-14 14:58:30 +0100

    migration fix for en_US

--- config/Migrations/20190211210824_AddFoodCoopShopInstancesMap.php
+++ config/Migrations/20190211210824_AddFoodCoopShopInstancesMap.php
@@ -12,7 +12,7 @@ class AddFoodCoopShopInstancesMap extends AbstractMigration
                 $sql = "INSERT INTO `fcs_configuration` (`id_configuration`, `active`, `name`, `text`, `value`, `type`, `position`, `locale`, `date_add`, `date_upd`) VALUES (NULL, '1', 'FCS_FOODCOOPS_MAP_ENABLED', 'Auf Home Karte mit anderen Foodcoops anzeigen?', '1', 'boolean', '128', 'de_DE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
                 break;
             case 'en_US':
-                $sql = "INSERT INTO `fcs_configuration` (`id_configuration`, `active`, `name`, `text`, `value`, `type`, `position`, `locale`, `date_add`, `date_upd`) VALUES (NULL, '1', 'FCS_FOODCOOPS_MAP_ENABLED', 'Show map with other foodcoops on home?', '1', 'readonly', 'boolean', '128, 'en_US', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+                $sql = "INSERT INTO `fcs_configuration` (`id_configuration`, `active`, `name`, `text`, `value`, `type`, `position`, `locale`, `date_add`, `date_upd`) VALUES (NULL, '1', 'FCS_FOODCOOPS_MAP_ENABLED', 'Show map with other foodcoops on home?',         '1', 'boolean', '128', 'en_US', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
                 break;
         }
         
