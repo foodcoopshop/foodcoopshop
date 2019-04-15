bfe44c2de735fedb1c9cbed4c84784af62263733
Mario Rothauer <marothauer@gmail.com>
2019-04-15 17:38:19 +0200

    margintop

--- src/Lib/Pdf/BarCodeTcpdf.php
+++ src/Lib/Pdf/BarCodeTcpdf.php
@@ -37,8 +37,7 @@ class BarCodeTcpdf extends AppTcpdf
     public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskcache = false, $pdfa = false)
     {
         parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
-//         $this->SetTopMargin(43);
-//         $this->SetRightMargin(0);
+        $this->SetTopMargin(58);
         $this->SetFontSize(10);
     }
     
