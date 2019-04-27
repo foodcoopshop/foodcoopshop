65f5e3856386f1f58eb922165dea33bd9cd67ad4
Mario Rothauer <marothauer@gmail.com>
2019-04-27 14:13:07 +0200

    fixed semicolon error

--- config/sql/_installation/clean-db-data-en_US.sql
+++ config/sql/_installation/clean-db-data-en_US.sql
@@ -114,7 +114,7 @@ INSERT INTO `fcs_configuration` VALUES
 (578,1,'FCS_WEEKLY_PICKUP_DAY','Weekly pickup day','5','readonly',60,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00'),
 (579,1,'FCS_DEFAULT_SEND_ORDER_LISTS_DAY_DELTA','Sending of order lists: x days before pickup day','2','readonly',65,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00'),
 (580,1,'FCS_ORDER_POSSIBLE_FOR_STOCK_PRODUCTS_IN_ORDERS_WITH_DELIVERY_RHYTHM','Allow weekly orders for stock products?','1','boolean',75,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00'),
-(581,1,'FCS_SHOW_NON_STOCK_PRODUCTS_IN_INSTANT_ORDERS','Only show stock products in instant orders?','0','boolean',76,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00');
+(581,1,'FCS_SHOW_NON_STOCK_PRODUCTS_IN_INSTANT_ORDERS','Only show stock products in instant orders?','0','boolean',76,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00'),
 (582,1,'FCS_INCLUDE_STOCK_PRODUCTS_IN_INVOICES','Include stock products in invoices?','1','readonly',60,'en_US','2019-02-18 12:38:00','2019-02-18 12:38:00'),
 (583,1,'FCS_REGISTRATION_NOTIFICATION_EMAILS','Who should be notified on new registrations?<br /><div class=\"small\">Please separate multiple e-mail addresses with , (no space).</div>','','text',55,'en_US','2019-03-05 20:01:59','2019-03-05 20:01:59'),
 (584,1,'FCS_SELF_SERVICE_MODE_FOR_STOCK_PRODUCTS_ENABLED','Use self-service mode for stock products?<br /><div class=\"small\"><a href=\"https://foodcoopshop.github.io/de/self-service-mode\" target=\"_blank\">Online documentation</a></div>','0','readonly',57,'en_US','2019-04-17 20:01:59','2019-04-17 20:01:59');
