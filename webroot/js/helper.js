82e6d649523f865b4ea11962fdce42cd11c441bf
Mario Rothauer <marothauer@gmail.com>
2019-07-28 20:54:48 +0200

    ckeditor v4.12.1

--- webroot/js/helper.js
+++ webroot/js/helper.js
@@ -701,7 +701,7 @@ foodcoopshop.Helper = {
 
         this.destroyCkeditor(name);
 
-        CKEDITOR.timestamp = 'v4.11.4-1';
+        CKEDITOR.timestamp = 'v4.12.1';
         $('textarea#' + name + '.ckeditor').ckeditor({
             customConfig: '/js/ckeditor/config.js'
         });
@@ -729,7 +729,7 @@ foodcoopshop.Helper = {
 
         this.destroyCkeditor(name);
 
-        CKEDITOR.timestamp = 'v4.11.4-1';
+        CKEDITOR.timestamp = 'v4.12.1';
         $('textarea#' + name + '.ckeditor').ckeditor({
             customConfig: '/js/ckeditor/config-big.js'
         });
@@ -744,7 +744,7 @@ foodcoopshop.Helper = {
 
         this.destroyCkeditor(name);
 
-        CKEDITOR.timestamp = 'v4.11.4-1';
+        CKEDITOR.timestamp = 'v4.12.1';
         $('textarea#' + name + '.ckeditor').ckeditor({
             customConfig: '/js/ckeditor/config-small-with-upload.js'
         });
