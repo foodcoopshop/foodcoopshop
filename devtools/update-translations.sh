1c5035f8ddf51bcc404ec9d580d702a043481cec
Mario Rothauer <office@rothauer-it.com>
2019-12-29 18:27:38 +0100

    resources/locales

--- devtools/update-translations.sh
+++ devtools/update-translations.sh
@@ -8,20 +8,20 @@ source $(dirname $0)/locales.sh
 bin/cake i18n extract --ignore-model-validation --output src\\Locale --paths src,config --overwrite --extract-core no --merge no --no-location --exclude plugins
 for locale in "${LOCALES[@]}"
 do
-    msgmerge src/Locale/$locale/cake.po src/Locale/cake.pot --output-file=src/Locale/$locale/cake.po --width=1000
-    msgmerge src/Locale/$locale/default.po src/Locale/default.pot --output-file=src/Locale/$locale/default.po --width=1000
+    msgmerge resources/locales/$locale/cake.po resources/locales/cake.pot --output-file=resources/locales/$locale/cake.po --width=1000
+    msgmerge resources/locales/$locale/default.po resources/locales/default.pot --output-file=resources/locales/$locale/default.po --width=1000
 done
 
 #get and merge translations for admin plugin
 bin/cake i18n extract --plugin Admin --ignore-model-validation --overwrite --extract-core no --merge yes --no-location
 for locale in "${LOCALES[@]}"
 do
-    msgmerge plugins/Admin/src/Locale/$locale/admin.po plugins/Admin/src/Locale/default.pot --output-file=plugins/Admin/src/Locale/$locale/admin.po --width=1000
+    msgmerge plugins/Admin/resources/locales/$locale/admin.po plugins/Admin/resources/locales/default.pot --output-file=plugins/Admin/resources/locales/$locale/admin.po --width=1000
 done
 
 #get and merge translations for network plugin
 bin/cake i18n extract --plugin Network --ignore-model-validation --overwrite --extract-core no --merge yes --no-location
 for locale in "${LOCALES[@]}"
 do
-    msgmerge plugins/Network/src/Locale/$locale/network.po plugins/Network/src/Locale/default.pot --output-file=plugins/Network/src/Locale/$locale/network.po --width=1000
+    msgmerge plugins/Network/resources/locales/$locale/network.po plugins/Network/resources/locales/default.pot --output-file=plugins/Network/resources/locales/$locale/network.po --width=1000
 done
