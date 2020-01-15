c1284032fb1a396a38b4331dc98e69d64b273a3d
Mario Rothauer <office@rothauer-it.com>
2020-01-15 16:12:41 +0100

    remove trash volume

--- config/elfinder/php/connector.minimal.php
+++ config/elfinder/php/connector.minimal.php
@@ -114,11 +114,11 @@ elFinder::$netDrivers['ftp'] = 'FTP';
  * @return bool|null
  **/
 function access($attr, $path, $data, $volume, $isDir, $relpath) {
-	$basename = basename($path);
-	return $basename[0] === '.'                  // if file/folder begins with '.' (dot)
-			 && strlen($relpath) !== 1           // but with out volume root
-		? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
-		:  null;                                 // else elFinder decide it itself
+    $basename = basename($path);
+    return $basename[0] === '.'                  // if file/folder begins with '.' (dot)
+    && strlen($relpath) !== 1           // but with out volume root
+    ? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
+    :  null;                                 // else elFinder decide it itself
 }
 
 
@@ -160,31 +160,19 @@ $opts = array(
     ),
     // 'debug' => true,
     'roots' => array(
-	    // Items volume
-	    array(
-			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
-		    'path'          => $uploadPath, // path to files (REQUIRED)
-			'URL'           => $uploadUrl, // URL to files (REQUIRED)
-	        'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
-	        'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-			'uploadAllow'   => $allowed,// Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
-			'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
-		),
-	    // Trash volume
-	    array(
-	        'id'            => '1',
-	        'driver'        => 'Trash',
-	        'path'          => '../files/.trash/',
-	        'tmbURL'        => $uploadPath . '/.trash/.tmb/',
-	        'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-	        'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
-	        'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain'), // Same as above
-	        'uploadOrder'   => array('deny', 'allow'),      // Same as above
-	        'accessControl' => 'access',                    // Same as above
-	    )
-	)
+        // Items volume
+        array(
+            'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
+            'path'          => $uploadPath, // path to files (REQUIRED)
+            'URL'           => $uploadUrl, // URL to files (REQUIRED)
+            'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
+            'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
+            'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
+            'uploadAllow'   => $allowed,// Mimetype `image` and `text/plain` allowed to upload
+            'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
+            'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
+        ),
+    )
 );
 
 // run elFinder
