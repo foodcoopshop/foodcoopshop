44727b33a86a7035349ccfa26a0e694e8c7af422
Mario Rothauer <office@rothauer-it.com>
2020-04-15 10:00:55 +0200

    show products on home (based on config)

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -30,9 +30,39 @@ echo $this->element('blogPosts', [
     'useCarousel' => false
 ]);
 
+if (!empty($newProducts)) {
+    
+    $this->element('addScript', ['script' =>
+        Configure::read('app.jsNamespace').".AppFeatherlight.addLightboxToCkeditorImages('.product-wrapper .toggle-content.description img');".
+        Configure::read('app.jsNamespace').".AppFeatherlight.initLightboxForImages('.product-wrapper a.lightbox');".
+        Configure::read('app.jsNamespace').".Helper.bindToggleLinks();".
+        Configure::read('app.jsNamespace').".Helper.initAmountSwitcher();".
+        Configure::read('app.jsNamespace').".Helper.initProductAttributesButtons();".
+        Configure::read('app.jsNamespace').".Cart.initAddToCartButton();".
+        Configure::read('app.jsNamespace').".Cart.initRemoveFromCartLinks();"
+    ]);
+    
+    echo $this->element('timebasedCurrency/addProductTooltip', ['selectorClass' => 'timebased-currency-product-info']);
+    
+    $isFirstElement = empty($blogPosts) || $blogPosts->count() == 0;
+    echo '<h1 style="float:left;' . (!$isFirstElement ? 'margin-top:20px;' : '') . '">';
+        echo __('New_products');
+    echo '</h2>';
+    
+    foreach ($newProducts as $product) {
+        echo $this->element('product/product', [
+            'product' => $product,
+            'showProductDetailLink' => true,
+            'showManufacturerDetailLink' => true,
+            'showIsNewBadgeAsLink' => true
+        ]);
+    }
+    
+}
+
 if (Configure::read('appDb.FCS_FOODCOOPS_MAP_ENABLED')) {
     echo $this->element('foodCoopShopInstancesMap', [
-        'isFirstElement' => empty($blogPosts) || $blogPosts->count() == 0
+        'isFirstElement' => (empty($blogPosts) || $blogPosts->count() == 0) && empty($newProducts)
     ]);
 }
 ?>
