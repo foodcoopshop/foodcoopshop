3829b6c5bb43964607f2211caa274fca18f69998
Mario Rothauer <office@rothauer-it.com>
2020-05-04 08:56:32 +0200

    customer dropdown was not loaded on click

--- plugins/Admin/templates/Reports/payments.php
+++ plugins/Admin/templates/Reports/payments.php
@@ -20,7 +20,8 @@ $this->element('addScript', [
         $('input.datepicker').datepicker();" .
         Configure::read('app.jsNamespace') . ".Admin.init();".
         Configure::read('app.jsNamespace') . ".Helper.initTooltip('.payment-approval-comment');".
-        Configure::read('app.jsNamespace') . ".Admin.selectMainMenuAdmin('".__d('admin', 'Website_administration')."', '".__d('admin', 'Financial_reports')."');"
+        Configure::read('app.jsNamespace') . ".Admin.selectMainMenuAdmin('".__d('admin', 'Website_administration')."', '".__d('admin', 'Financial_reports')."');".
+        Configure::read('app.jsNamespace') . ".Admin.initCustomerDropdown(" . ($customerId != '' ? $customerId : '0') . ");"
 ]);
 if ($paymentType == 'product') {
     $this->element('highlightRowAfterEdit', [
