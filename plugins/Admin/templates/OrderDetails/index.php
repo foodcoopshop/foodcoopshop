ec33722a593709b7c8b86c33892e699d36af84a4
Mario Rothauer <office@rothauer-it.com>
2020-06-18 09:48:28 +0200

    add modal for feedback add

--- plugins/Admin/templates/OrderDetails/index.php
+++ plugins/Admin/templates/OrderDetails/index.php
@@ -35,6 +35,7 @@ use Cake\Core\Configure;
         $this->element('addScript', [
             'script' =>
             Configure::read('app.jsNamespace').".ModalOrderDetailDelete.init();" .
+            Configure::read('app.jsNamespace').".ModalOrderDetailFeedbackAdd.init();" .
             Configure::read('app.jsNamespace').".ModalOrderDetailProductPriceEdit.init();" .
             Configure::read('app.jsNamespace').".ModalOrderDetailProductQuantityEdit.init();" .
             Configure::read('app.jsNamespace').".Helper.initTooltip('.product-feedback-button');" .
