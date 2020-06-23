3b9d68b4433a90feb04aca99f75babd314e3571e
Mario Rothauer <office@rothauer-it.com>
2020-06-23 11:18:09 +0200

    add hofladen online string replacements

---
+++ src/Lib/OutputFilter/config/de_DE/hofladenOnlineConfig.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 3.1.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+
+return [
+    'der Foodcoop' => 'des Hofladens',
+    'zur Foodcoop' => 'zum Hofladen',
+    'die Foodcoop' => 'der Hofladen',
+    'für die gesamte Foodcoop' => 'für den gesamten Hofladen',
+    'Version FoodCoopShop' => 'Version Hofladen-Online',
+    'in den FoodCoopShop' => 'in Hofladen-Online',
+    'foodcoopshop.com' => 'hofladen-online.at',
+    'FoodCoopShop' => 'Hofladen-Online',
+    'Foodcoop Software' => 'Software für Direktvermarkter',
+    'Lieferpause für alle Hersteller?' => 'Lieferpause für den gesamten Hofladen?',
+    '<h2>Betreiber der Plattform</h2>' => '<h2>Betreiber der Plattform (= Hersteller)</h2>',
+    'im Folgenden kurz: Betreiber' => 'im Folgenden kurz: Betreiber bzw. Hersteller',
+    'Der Betreiber stellt dem Nutzer eine Plattform unentgeltlich zur Verfügung, auf der Hersteller Waren und Dienstleistungen präsentieren. Diese dargebotenen Waren und Dienstleistungen sind eine unverbindliche Aufforderung des jeweils genannten Herstellers an den Nutzer, ein verbindliches Anbot für die angebotenen Waren und Dienstleistungen zu legen. Durch die Bestellung legt der Nutzer ein solches verbindliches Anbot an den jeweils genannten Hersteller.' => 'Der Betreiber stellt dem Nutzer eine Plattform unentgeltlich zur Verfügung, auf der Waren und Dienstleistungen präsentiert werden. Diese dargebotenen Waren und Dienstleistungen sind eine unverbindliche Aufforderung an den Nutzer, ein verbindliches Anbot für die angebotenen Waren und Dienstleistungen zu legen. Durch die Bestellung legt der Nutzer ein solches verbindliches Anbot an den Hersteller.',
+    'Der Vertrag über die Waren und Dienstleistungen kommt ausschließlich zwischen dem Nutzer und dem jeweiligen Hersteller zustande (Direktverkauf).' => 'Der Vertrag über die Waren und Dienstleistungen kommt zwischen dem Nutzer und dem Hersteller zustande (Direktverkauf).',
+    'mit dem jeweiligen Hersteller' => 'mit dem Hersteller',
+    'Der jeweilige Hersteller' => 'Der Hersteller',
+    '<p>4.3 Die E-Mailadresse des jeweiligen Herstellers ergibt sich aus dessen Impressum.</p>' => '',
+    '_FOODCOOPSHOP_' => '_HOFLADEN_ONLINE_',
+    'FCS_' => 'HOF_',
+    'href="https://foodcoopshop.github.io/(.*)"' => 'href="https://www.hofladen-online.at/hilfe" data-url="/$1"',
+    'https://foodcoopshop.github.io/de/produkte"' => 'https://www.hofladen-online.at/hilfe"',
+    'https://foodcoopshop.github.io/de/netzwerk-modul"' => 'https://www.hofladen-online.at/hilfe"',
+    'https://foodcoopshop.github.io/de/selbstbedienungs-modus"' => 'https://www.hofladen-online.at/hilfe"',
+];
