c9b9fec5b0edc348959eaa07b5a4629bfe0f5cda
Mario Rothauer <office@rothauer-it.com>
2020-06-28 22:24:27 +0200

    sort customer by id

--- plugins/Admin/src/Controller/CustomersController.php
+++ plugins/Admin/src/Controller/CustomersController.php
@@ -728,7 +728,7 @@ class CustomersController extends AdminAppController
 
         $customers = $this->paginate($query, [
             'sortWhitelist' => [
-                'Customers.' . Configure::read('app.customerMainNamePart'), 'Customers.id_default_group', 'Customers.id_default_group', 'Customers.email', 'Customers.active', 'Customers.email_order_reminder', 'Customers.date_add', 'Customers.timebased_currency_enabled'
+                'Customers.' . Configure::read('app.customerMainNamePart'), 'Customers.id_default_group', 'Customers.id_customer', 'Customers.email', 'Customers.active', 'Customers.email_order_reminder', 'Customers.date_add', 'Customers.timebased_currency_enabled'
             ],
             'order' => [
                 'Customers.' . Configure::read('app.customerMainNamePart') => 'ASC'
