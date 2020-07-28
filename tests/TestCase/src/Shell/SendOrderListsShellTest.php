d40e461573df04980f6501d76141abf9bd15bdef
Mario Rothauer <office@rothauer-it.com>
2020-07-28 22:05:11 +0200

    Revert "do not skip any more"
    This reverts commit fd2e034f842ffe03962602d2300167ec1556f460.

--- tests/TestCase/src/Shell/SendOrderListsShellTest.php
+++ tests/TestCase/src/Shell/SendOrderListsShellTest.php
@@ -322,6 +322,7 @@ class SendOrderListsShellTest extends AppCakeTestCase
 
     public function testContentOfOrderList()
     {
+        $this->markTestSkipped('output needs to be removed');
         $this->loginAsSuperadmin();
         $this->get('/admin/manufacturers/getOrderListByProduct.pdf?manufacturerId=4&pickupDay=02.02.2018&outputType=html');
         $expectedResult = file_get_contents(TESTS . 'config' . DS . 'data' . DS . 'orderList.html');
