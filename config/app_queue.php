0250bb178967a107b533837cbef9d73dcda55ec2
Mario Rothauer <office@rothauer-it.com>
2020-09-18 09:13:18 +0200

    5 min

--- config/app_queue.php
+++ config/app_queue.php
@@ -24,7 +24,7 @@ return [
     'Queue' => [
         'maxworkers' => 1,
         'defaultworkerretries' => 2,
-        'workermaxruntime' => 35,
+        'workermaxruntime' => 300,
         'sleeptime' => 20,
         'connection' => $connection,
         'exitwhennothingtodo' => $exitwhennothingtodo,
