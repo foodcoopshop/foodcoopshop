6d27d67509ec56ed83d71ebbd236def048a09c1a
Mario Rothauer <office@rothauer-it.com>
2020-09-21 08:38:44 +0200

    changed icons

--- src/Shell/SendOrderListsShell.php
+++ src/Shell/SendOrderListsShell.php
@@ -156,7 +156,7 @@ class SendOrderListsShell extends AppShell
                         $pickupDayFormated = new FrozenDate($pickupDayDbFormat);
                         $pickupDayFormated = $pickupDayFormated->i18nFormat(Configure::read('app.timeHelper')->getI18Format('DateLong2'));
                         $identifier = $manufacturer->id_manufacturer . '-' . $pickupDayFormated;
-                        $newData = '- <i class="far fa-file-pdf not-ok" data-identifier="generate-order-list-'.$identifier.'"></i> <i class="far fa-envelope not-ok" data-identifier="send-order-list-'.$identifier.'"></i> ';
+                        $newData = '- <i class="fas fa-book not-ok" data-identifier="generate-order-list-'.$identifier.'"></i> <i class="fas fa-envelope not-ok" data-identifier="send-order-list-'.$identifier.'"></i> ';
                         $newData .= html_entity_decode($manufacturer->name) . ': ' .
                             __('{0,plural,=1{1_product} other{#_products}}', [$tmpActionLogData['order_detail_amount_sum']]) . ' / ' .
                             Configure::read('app.numberHelper')->formatAsCurrency($tmpActionLogData['order_detail_price_sum']);
