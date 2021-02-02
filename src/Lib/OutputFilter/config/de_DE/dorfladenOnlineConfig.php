4d8fbb2dd2817d31f9cc1d473518ac2c0ae700c8
Mario Rothauer <office@rothauer-it.com>
2021-02-02 11:02:49 +0100

    add dorfladen online replacements

---
+++ src/Lib/OutputFilter/config/de_DE/dorfladenOnlineConfig.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 3.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+
+return [
+    'Version FoodCoopShop' => 'Version Dorfladen Online',
+    'in den FoodCoopShop' => 'in Dorfladen Online',
+    'foodcoopshop.com' => 'dorfladen-online.at',
+    'FoodCoopShop' => 'Dorfladen Online',
+    'Foodcoop Software' => 'Bestell-Software für den Lebensmittel-Einzelhandel',
+    '_FOODCOOPSHOP_' => '_DORFLADEN_ONLINE_',
+    'href="https://foodcoopshop.github.io/(.*)"' => 'href="https://www.dorfladen-online.at/hilfe" data-url="/$1"',
+    'https://foodcoopshop.github.io/de/produkte"' => 'https://www.dorfladen-online.at/hilfe"',
+    'https://foodcoopshop.github.io/de/netzwerk-modul"' => 'https://www.dorfladen-online.at/hilfe"',
+    'https://foodcoopshop.github.io/de/selbstbedienungs-modus"' => 'https://www.dorfladen-online.at/hilfe"',
+];
