a27d928700ee00347e4e27123b74fb41462f52e6
Mario Rothauer <office@rothauer-it.com>
2021-03-08 09:47:02 +0100

    remove .git folder

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -3,6 +3,7 @@
 bash devtools/setup-git.sh
 
 ## remove overhead from tcpdf library
+rm -Rf vendor/tecnickcom/.git
 rm -Rf vendor/tecnickcom/tcpdf/examples
 rm -Rf vendor/tecnickcom/tcpdf/fonts/ae_fonts_2.0
 rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.33
