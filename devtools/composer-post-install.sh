8cc235aea657bf585b4e0a3f412f8c70635803f7
Mario Rothauer <office@rothauer-it.com>
2021-03-29 09:19:07 +0200

    .git folder not existing any more

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -3,7 +3,6 @@
 bash devtools/setup-git.sh
 
 ## remove overhead from tcpdf library
-rm -Rf vendor/tecnickcom/tcpdf/.git
 rm -Rf vendor/tecnickcom/tcpdf/examples
 rm -Rf vendor/tecnickcom/tcpdf/fonts/ae_fonts_2.0
 rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.33
