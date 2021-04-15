d6e594548fadbc2ecf3384a2ebd1304cae5a58e3
Mario Rothauer <office@rothauer-it.com>
2021-04-15 08:26:51 +0200

    add comment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,4 +11,6 @@ require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_SERVER['PHP_SELF'] = '/';
 
+// phpunit with enabled processIsolation sends headers before output
+// https://github.com/cakephp/docs/pull/6988
 session_id('cli');
