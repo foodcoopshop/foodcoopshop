86206d058d6434a989abb2c4d35784b2637088a0
Mario Rothauer <office@rothauer-it.com>
2021-05-05 16:35:07 +0200

    chore: add offset to avoid memory overflow

--- config/Migrations/20210504085123_SaveTaxInOrderDetails.php
+++ config/Migrations/20210504085123_SaveTaxInOrderDetails.php
@@ -13,35 +13,51 @@ class SaveTaxInOrderDetails extends AbstractMigration
 
         $this->OrderDetail = FactoryLocator::get('Table')->get('OrderDetails');
 
-        $orderDetails = $this->OrderDetail->find('all')->toArray();
+        $orderDetailCountPerMigrationStep = 1000;
+        $orderDetails = $this->OrderDetail->find('all');
+        $loopsCount = ceil($orderDetails->count() / $orderDetailCountPerMigrationStep);
 
-        $i= 0;
-        foreach($orderDetails as $orderDetail) {
+        $i = 0;
+        while($i <= $loopsCount) {
 
-            $sql = "SELECT t.rate FROM fcs_tax t LEFT JOIN fcs_order_detail od ON t.id_tax = od.id_tax WHERE od.id_order_detail = :orderDetailId";
-            $statement = $this->OrderDetail->getConnection()->prepare($sql);
-            $params = ['orderDetailId' => $orderDetail->id_order_detail];
-            $statement->execute($params);
-            $taxes =  $statement->fetchAll('assoc');
+            $orderDetails = $this->OrderDetail->find('all', [
+                'limit' => $orderDetailCountPerMigrationStep,
+                'offset' => $i * $orderDetailCountPerMigrationStep,
+            ])->toArray();
 
-            if (!empty($taxes)) {
-                $orderDetails[$i]->tax_rate = $taxes[0]['rate'];
+            $j = 0;
+            foreach($orderDetails as $orderDetail) {
+
+                $sql = "SELECT t.rate FROM fcs_tax t LEFT JOIN fcs_order_detail od ON t.id_tax = od.id_tax WHERE od.id_order_detail = :orderDetailId";
+                $statement = $this->OrderDetail->getConnection()->prepare($sql);
+                $params = ['orderDetailId' => $orderDetail->id_order_detail];
+                $statement->execute($params);
+                $taxes =  $statement->fetchAll('assoc');
+
+                if (!empty($taxes)) {
+                    $orderDetails[$j]->tax_rate = $taxes[0]['rate'];
+                }
+
+                $sql = "SELECT odt.* FROM fcs_order_detail od LEFT JOIN fcs_order_detail_tax odt ON odt.id_order_detail = od.id_order_detail WHERE od.id_order_detail = :orderDetailId";
+                $statement = $this->OrderDetail->getConnection()->prepare($sql);
+                $params = ['orderDetailId' => $orderDetail->id_order_detail];
+                $statement->execute($params);
+                $orderDetailTaxes =  $statement->fetchAll('assoc');
+
+                if (!empty($orderDetailTaxes)) {
+                    $orderDetails[$j]->tax_unit_amount = $orderDetailTaxes[0]['unit_amount'] ?? 0;
+                    $orderDetails[$j]->tax_total_amount = $orderDetailTaxes[0]['total_amount'] ?? 0;
+                }
+
+                $j++;
+
             }
 
-            $sql = "SELECT odt.* FROM fcs_order_detail od LEFT JOIN fcs_order_detail_tax odt ON odt.id_order_detail = od.id_order_detail WHERE od.id_order_detail = :orderDetailId";
-            $statement = $this->OrderDetail->getConnection()->prepare($sql);
-            $params = ['orderDetailId' => $orderDetail->id_order_detail];
-            $statement->execute($params);
-            $orderDetailTaxes =  $statement->fetchAll('assoc');
+            $this->OrderDetail->saveMany($orderDetails);
 
-            if (!empty($orderDetailTaxes)) {
-                $orderDetails[$i]->tax_unit_amount = $orderDetailTaxes[0]['unit_amount'] ?? 0;
-                $orderDetails[$i]->tax_total_amount = $orderDetailTaxes[0]['total_amount'] ?? 0;
-            }
+            echo "- Tax for " . $orderDetailCountPerMigrationStep . " order details (starting with id " . $i * $orderDetailCountPerMigrationStep. ") migrated successfully.\n";
             $i++;
         }
-
-        $this->OrderDetail->saveMany($orderDetails);
 
         $this->execute("DROP TABLE `fcs_order_detail_tax`;");
         $this->execute("ALTER TABLE `fcs_order_detail` DROP `id_tax`;");
