b3ac84fbe1a2a962e96c70338e6a7762e32b8e05
Mario Rothauer <office@rothauer-it.com>
2021-08-26 13:44:58 +0200

    only in retail mode

--- src/Controller/SelfServiceController.php
+++ src/Controller/SelfServiceController.php
@@ -78,21 +78,25 @@ class SelfServiceController extends FrontendController
             }
         }
 
-        $this->Cart = $this->getTableLocator()->get('Carts');
-        $defaultSelfServicePaymentType = $this->Cart::CART_SELF_SERVICE_PAYMENT_TYPE_CREDIT;
-        if ($this->AppAuth->isSelfServiceCustomer()) {
-            $defaultSelfServicePaymentType = $this->Cart::CART_SELF_SERVICE_PAYMENT_TYPE_CASH;
+        if (Configure::read('appDb.FCS_SEND_INVOICES_TO_CUSTOMERS')) {
+            $this->Cart = $this->getTableLocator()->get('Carts');
+            $defaultSelfServicePaymentType = $this->Cart::CART_SELF_SERVICE_PAYMENT_TYPE_CREDIT;
+            if ($this->AppAuth->isSelfServiceCustomer()) {
+                $defaultSelfServicePaymentType = $this->Cart::CART_SELF_SERVICE_PAYMENT_TYPE_CASH;
+            }
         }
         if ($this->getRequest()->getEnv('ORIGINAL_REQUEST_METHOD') == 'GET') {
             $cart = $this->AppAuth->getCart();
             $this->set('cart', $cart['Cart']);
-            $this->request = $this->request->withData('Carts.self_service_payment_type', $defaultSelfServicePaymentType);
+            if (Configure::read('appDb.FCS_SEND_INVOICES_TO_CUSTOMERS')) {
+                $this->request = $this->request->withData('Carts.self_service_payment_type', $defaultSelfServicePaymentType);
+            }
         }
 
         if ($this->getRequest()->getEnv('ORIGINAL_REQUEST_METHOD') == 'POST') {
 
             // data gets lost form element if it is disabled
-            if ($this->AppAuth->isSelfServiceCustomer()) {
+            if (Configure::read('appDb.FCS_SEND_INVOICES_TO_CUSTOMERS') && $this->AppAuth->isSelfServiceCustomer()) {
                 $this->request = $this->request->withData('Carts.self_service_payment_type', $defaultSelfServicePaymentType);
             }
             if ($this->AppAuth->Cart->isCartEmpty()) {
