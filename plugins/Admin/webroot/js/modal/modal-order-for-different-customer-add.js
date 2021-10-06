98d18600abb241d7ab8f5dd2244ca2fa42bd91cb
Mario Rothauer <office@rothauer-it.com>
2021-10-06 08:53:44 +0200

    fix: layout fix for dropdown in header of order for different customer

--- plugins/Admin/webroot/js/modal/modal-order-for-different-customer-add.js
+++ plugins/Admin/webroot/js/modal/modal-order-for-different-customer-add.js
@@ -69,7 +69,7 @@ foodcoopshop.ModalIOrderForDifferentCustomerAdd = {
 
         // START DROPDOWN
         var customerDropdownId = 'customerDropdown';
-        var header = $('<div class="message-container"><span class="start">' + foodcoopshop.LocalizedJs.admin.PlaceOrderFor + ': <select id="' + customerDropdownId + '"><option value="0">' + foodcoopshop.LocalizedJs.admin.PleaseSelect + '</option></select></span></div>');
+        var header = $('<div class="message-container"><span class="start"><span class="title">' + foodcoopshop.LocalizedJs.admin.PlaceOrderFor + ': </span><select id="' + customerDropdownId + '"><option value="0">' + foodcoopshop.LocalizedJs.admin.PleaseSelect + '</option></select></span></div>');
         $(modalSelector + ' .modal-title').append(header);
 
         var customerDropdownSelector = '#' + customerDropdownId;
