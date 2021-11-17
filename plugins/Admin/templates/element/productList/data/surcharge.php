33c1bb4c67feb8323780710b07ffe3ee14db4bc6
Mario Rothauer <office@rothauer-it.com>
2021-11-17 08:47:55 +0100

    never show 0 surcharge

--- plugins/Admin/templates/element/productList/data/surcharge.php
+++ plugins/Admin/templates/element/productList/data/surcharge.php
@@ -15,7 +15,7 @@
 
 echo '<td class="cell-surcharge">';
     if (empty($product->product_attributes)) {
-        if (isset($product->surcharge_percent)) {
+        if (isset($product->surcharge_percent) && $product->surcharge_percent > 0) {
             echo '<b>' . $this->Number->formatAsPercent($product->surcharge_percent, 1) . '</b><br />';
             echo $this->Number->formatAsCurrency($product->surcharge_price);
         }
