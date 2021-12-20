3beec9c04b2cba3a8f04b872b9719187c44f02fa
Mario Rothauer <office@rothauer-it.com>
2021-12-20 16:05:26 +0100

    modal for changing name of ordered product

---
+++ plugins/Admin/webroot/js/modal/modal-order-detail-product-name-edit.js
@@ -0,0 +1,94 @@
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 3.1.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+foodcoopshop.ModalOrderDetailProductNameEdit = {
+
+    init : function() {
+
+        var modalSelector = '#order-detail-product-name-edit-form';
+
+        foodcoopshop.Modal.appendModalToDom(
+            modalSelector,
+            foodcoopshop.LocalizedJs.admin.ChangeProductName,
+            foodcoopshop.ModalOrderDetailProductNameEdit.getHtml()
+        );
+
+        foodcoopshop.Modal.bindSuccessButton(modalSelector, function() {
+            foodcoopshop.ModalOrderDetailProductNameEdit.getSuccessHandler(modalSelector);
+        });
+
+        $(modalSelector).on('hidden.bs.modal', function (e) {
+            foodcoopshop.ModalOrderDetailProductNameEdit.getCloseHandler();
+        });
+
+        $('.order-detail-product-name-edit-button').on('click', function() {
+            foodcoopshop.ModalOrderDetailProductNameEdit.getOpenHandler($(this), modalSelector);
+        });
+
+    },
+
+    getHtml : function() {
+        var html = '<label for="dialogName"><b>' + foodcoopshop.LocalizedJs.dialogProduct.Name + '</b></label><br />';
+        html += '<input type="text" name="dialogOrderDetailProductNameName" id="dialogOrderDetailProductNameName" value="" /><br />';
+        html += '<input type="hidden" name="dialogOrderDetailProductNameOrderDetailId" id="dialogOrderDetailProductNameOrderDetailId" value="" />';
+        html += '<hr />';
+        return html;
+    },
+
+    getCloseHandler : function() {
+        $('#dialogOrderDetailProductNameName').val('');
+        $('#dialogOrderDetailProductNameOrderDetailId').val('');
+        $('#flashMessage').remove();
+    },
+
+    getSuccessHandler : function(modalSelector) {
+
+        var productName = $('#dialogOrderDetailProductNameName').val();
+        if ($('#dialogOrderDetailProductNameOrderDetailId').val() == '') {
+            return false;
+        }
+
+        foodcoopshop.Helper.ajaxCall(
+            '/admin/order-details/editProductName/',
+            {
+                orderDetailId: $('#dialogOrderDetailProductNameOrderDetailId').val(),
+                productName: productName,
+            },
+            {
+                onOk: function (data) {
+                    document.location.reload();
+                },
+                onError: function (data) {
+                    foodcoopshop.Modal.appendFlashMessage(modalSelector, data.msg);
+                    foodcoopshop.Modal.resetButtons(modalSelector);
+                }
+            }
+        );
+
+    },
+
+    getOpenHandler : function(button, modalSelector) {
+
+        var row = button.closest('tr');
+        var orderDetailId = row.find('td:nth-child(2)').html();
+        var productName = row.find('td:nth-child(4) a.name-for-dialog').text().trim();
+
+        $(modalSelector + ' #dialogOrderDetailProductNameOrderDetailId').val(orderDetailId);
+        $(modalSelector + ' #dialogOrderDetailProductNameName').val(productName);
+        $(modalSelector).modal();
+
+        $('#dialogOrderDetailProductNameName').focus();
+
+    }
+
+};
