0d2726474b158ebb34d52a6bc6efe452980ba91a
Mario Rothauer <office@rothauer-it.com>
2021-12-29 20:09:32 +0100

    use entity instead of custom query to trigger event afterSave

--- plugins/Admin/src/Controller/ProductsController.php
+++ plugins/Admin/src/Controller/ProductsController.php
@@ -942,26 +942,36 @@ class ProductsController extends AdminAppController
 
         $this->CategoryProduct = $this->getTableLocator()->get('CategoryProducts');
         $this->CategoryProduct->deleteAll([
-            'id_product' => $productId
+            'id_product' => $productId,
         ]);
 
         $this->Category = $this->getTableLocator()->get('Categories');
         $selectedCategoryNames = [];
-        foreach ($selectedCategories as $selectedCategory) {
+        $data = [];
+        foreach ($selectedCategories as $selectedCategoryId) {
             // only add if entry of passed id exists in category table
             $oldCategory = $this->Category->find('all', [
                 'conditions' => [
-                    'Categories.id_category' => $selectedCategory
+                    'Categories.id_category' => $selectedCategoryId
                 ]
             ])->first();
             if (! empty($oldCategory)) {
                 // do not track "all-products"
-                if ($selectedCategory != Configure::read('app.categoryAllProducts')) {
+                if ($selectedCategoryId != Configure::read('app.categoryAllProducts')) {
                     $selectedCategoryNames[] = $oldCategory->name;
                 }
-                $sql = 'INSERT INTO ' . $this->CategoryProduct->getTable() . ' (`id_product`, `id_category`) VALUES(' . $productId . ', ' . $selectedCategory . ');';
-                $this->CategoryProduct->getConnection()->query($sql);
+                $data[] = [
+                    'id_product' => $productId,
+                    'id_category' => $selectedCategoryId,
+                ];
             }
+        }
+        if (!empty($data)) {
+            $tmpPrimaryKey = $this->CategoryProduct->getPrimaryKey();
+            $this->CategoryProduct->setPrimaryKey(null);
+            $categoryProducts = $this->CategoryProduct->newEntities($data);
+            $this->CategoryProduct->saveMany($categoryProducts);
+            $this->CategoryProduct->setPrimaryKey($tmpPrimaryKey);
         }
 
         $messageString = __d('admin', 'The_categories_of_the_product_{0}_from_manufacturer_{1}_have_been_changed:_{2}', ['<b>' . $oldProduct->name . '</b>', '<b>' . $oldProduct->manufacturer->name . '</b>', join(', ', $selectedCategoryNames)]);
