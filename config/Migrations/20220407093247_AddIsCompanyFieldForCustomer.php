c4d36c809e9348c6585fd3b7a0de5a79641d0744
Mario Rothauer <office@rothauer-it.com>
2022-04-07 22:22:11 +0200

    increase field size to fit long company names

--- config/Migrations/20220407093247_AddIsCompanyFieldForCustomer.php
+++ config/Migrations/20220407093247_AddIsCompanyFieldForCustomer.php
@@ -7,7 +7,15 @@ class AddIsCompanyFieldForCustomer extends AbstractMigration
 {
     public function change()
     {
+
         $sql = "ALTER TABLE `fcs_customer` ADD `is_company` TINYINT NOT NULL DEFAULT '0' AFTER `id_default_group`;";
         $this->execute($sql);
+
+        $sql = "ALTER TABLE `fcs_customer` CHANGE `firstname` `firstname` VARCHAR(50) NOT NULL DEFAULT '', CHANGE `lastname` `lastname` VARCHAR(50) NOT NULL DEFAULT '';";
+        $this->execute($sql);
+
+        $sql = "ALTER TABLE `fcs_address` CHANGE `lastname` `lastname` VARCHAR(50) NOT NULL DEFAULT '', CHANGE `firstname` `firstname` VARCHAR(50) NOT NULL DEFAULT '';";
+        $this->execute($sql);
+
     }
 }
