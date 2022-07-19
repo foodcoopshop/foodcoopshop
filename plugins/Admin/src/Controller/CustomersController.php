ae4125bf359b3c8e47880ef818d68eccb153a2b5
Mario Rothauer <office@rothauer-it.com>
2022-07-19 21:53:56 +0200

    set original referer

--- plugins/Admin/src/Controller/CustomersController.php
+++ plugins/Admin/src/Controller/CustomersController.php
@@ -412,7 +412,7 @@ class CustomersController extends AdminAppController
             ]
         ])->first();
 
-        $this->setCurrentFormAsFormReferer();
+        $this->setFormReferer();
 
         if (empty($this->getRequest()->getData())) {
             $this->set('customer', $customer);
