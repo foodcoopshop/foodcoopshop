8c694ada072902c4eb779a695d425a292b6cce8d
Mario Rothauer <office@rothauer-it.com>
2022-08-16 09:21:50 +0200

    shrink release

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -2,12 +2,11 @@
 
 bash devtools/setup-git.sh
 
-## remove overhead from tcpdf library
+# remove overhead from tcpdf library
 rm -Rf vendor/tecnickcom/tcpdf/examples
-rm -Rf vendor/tecnickcom/tcpdf/fonts/ae_fonts_2.0
-rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.33
-rm -Rf vendor/tecnickcom/tcpdf/fonts/free*
-rm -Rf vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.34/status.txt
+
+#delete all files and folders in folder /fonts except starting with helvetica*
+find vendor/tecnickcom/tcpdf/fonts/ ! -name helvetica* -delete
 
 cp src/Assets/Filter/CleanCss.php vendor/markstory/mini-asset/src/Filter/
 
