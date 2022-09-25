e7bb29c5df50548f7274b0bd3c042e41687fbacf
Mario Rothauer <office@rothauer-it.com>
2022-09-25 20:14:19 +0200

    file renamings

--- devtools/setup-dev-init.sh
+++ devtools/setup-dev-init.sh
@@ -3,7 +3,7 @@
 docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./config/sql/_installation/clean-db-structure.sql
 docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./tests/config/sql/test-db-data.sql
 docker compose run --rm composer install
-bash ./devtools/set-permissions.sh
-bash ./devtools/setup-dev-config.sh
+bash ./devtools/setup-dev/set-permissions.sh
+bash ./devtools/setup-dev/config-files.sh
 docker compose run -w /var/www/html/webroot --rm node npm install
 docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake npm_post_install
