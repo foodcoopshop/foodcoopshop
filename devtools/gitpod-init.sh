64dea20aa78fe2f65551104e9e2f22ca4d204589
Mario Rothauer <office@rothauer-it.com>
2022-09-25 13:26:30 +0200

    gitpod init

---
+++ devtools/gitpod-init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./config/sql/_installation/clean-db-structure.sql
+docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./tests/config/sql/test-db-data.sql
+docker compose run --rm composer install
+bash ./devtools/set-permissions.sh
+bash ./devtools/setup-dev-config.sh
+docker compose run -w /var/www/html/webroot --rm node npm install
+docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake npm_post_install
