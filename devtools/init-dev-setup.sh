f95a8207560cfde0ce5c4b091890010d0ff04767
Mario Rothauer <office@rothauer-it.com>
2022-09-28 16:32:27 +0200

    seed

--- devtools/init-dev-setup.sh
+++ devtools/init-dev-setup.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-CURRENT_UID=$(id -u):$(id -g) docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./config/sql/_installation/clean-db-structure.sql
-CURRENT_UID=$(id -u):$(id -g) docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./tests/config/sql/test-db-data.sql
 CURRENT_UID=$(id -u):$(id -g) docker compose run --rm composer install
 bash ./devtools/setup-dev/set-permissions.sh
 bash ./devtools/setup-dev/copy-config-files.sh
 docker compose run -w /var/www/html/webroot --rm node npm install
 CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations seed --source Seeds/tests
