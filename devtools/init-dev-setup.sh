f8185bf6bd7b2352d5ad998f66c24c501ba424b3
Mario Rothauer <office@rothauer-it.com>
2022-09-28 16:54:51 +0200

    fix

--- devtools/init-dev-setup.sh
+++ devtools/init-dev-setup.sh
@@ -3,7 +3,7 @@
 CURRENT_UID=$(id -u):$(id -g) docker compose run --rm composer install
 bash ./devtools/setup-dev/set-permissions.sh
 bash ./devtools/setup-dev/copy-config-files.sh
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate -P queue
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate -p Queue
 CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate
 CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations seed --source Seeds/tests
 docker compose run -w /var/www/html/webroot --rm node npm install
