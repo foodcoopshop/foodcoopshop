06192c881c9d9a8dd6097e5bb723c6c44dbdc1c7
Mario Rothauer <office@rothauer-it.com>
2022-09-28 14:20:45 +0200

    fixes rounding problems in unit test

--- src/Model/Table/InvoicesTable.php
+++ src/Model/Table/InvoicesTable.php
@@ -116,6 +116,13 @@ class InvoicesTable extends AppTable
                     $taxRatesSums[$trt]['sum_price_incl'] += $invoiceTax->total_price_tax_incl;
                 }
 
+                $taxRates[$trt][$taxRate]['sum_price_excl'] = round($taxRates[$trt][$taxRate]['sum_price_excl'], 2);
+                $taxRates[$trt][$taxRate]['sum_tax'] = round($taxRates[$trt][$taxRate]['sum_tax'], 2);
+                $taxRates[$trt][$taxRate]['sum_price_incl'] = round($taxRates[$trt][$taxRate]['sum_price_incl'], 2);
+                $taxRatesSums[$trt]['sum_price_excl'] = round($taxRatesSums[$trt]['sum_price_excl'], 2);
+                $taxRatesSums[$trt]['sum_tax'] = round($taxRatesSums[$trt]['sum_tax'], 2);
+                $taxRatesSums[$trt]['sum_price_incl'] = round($taxRatesSums[$trt]['sum_price_incl'], 2);
+
             }
         }
 
