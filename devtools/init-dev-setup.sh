adfaeb557d9084765cfe9f255a5afff895a2a5b7
Mario Rothauer <office@rothauer-it.com>
2022-10-02 22:47:04 +0200

    revert

--- devtools/init-dev-setup.sh
+++ devtools/init-dev-setup.sh
@@ -2,7 +2,7 @@
 
 CURRENT_UID=$(id -u):$(id -g) docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./config/sql/_installation/clean-db-structure.sql
 CURRENT_UID=$(id -u):$(id -g) docker compose exec -T database-dev mysql --port 3310 foodcoopshop-dev < ./tests/config/sql/test-db-data.sql
-CURRENT_UID=$(id -u):$(id -g) docker compose run --rm composer install --ignore-platform-reqs
+CURRENT_UID=$(id -u):$(id -g) docker compose run --rm composer install
 bash ./devtools/setup-dev/set-permissions.sh
 bash ./devtools/setup-dev/copy-config-files.sh
 docker compose run -w /var/www/html/webroot --rm node npm install
