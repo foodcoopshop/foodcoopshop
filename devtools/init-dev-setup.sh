e91bd5f882af0a3c4a343f9e8ef24f5137c24ab8
Mario Rothauer <office@rothauer-it.com>
2022-10-03 15:12:11 +0200

    fixes

--- devtools/init-dev-setup.sh
+++ devtools/init-dev-setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate -p Queue
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations seed --source Seeds/tests --seed InitTestDataSeed
-
 CURRENT_UID=$(id -u):$(id -g) docker compose run --rm composer install
+
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate -p Queue
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate --source Migrations/init
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations seed --source Seeds/tests --seed InitTestDataSeed
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /var/www/html fcs-php-nginx bash ./bin/cake migrations migrate
+
 bash ./devtools/setup-dev/set-permissions.sh
 bash ./devtools/setup-dev/copy-config-files.sh
 docker compose run -w /var/www/html/webroot --rm node npm install
