cdb48fb709fa44ebf6b9b81d057a7b0479891dae
Mario Rothauer <office@rothauer-it.com>
2022-10-11 16:57:03 +0200

    adapt script

--- devtools/installation/init-database.sh
+++ devtools/installation/init-database.sh
@@ -15,6 +15,6 @@ fi
 
 ./bin/cake migrations migrate -p Queue
 ./bin/cake migrations migrate --source Migrations/init
+./bin/cake migrations migrate
 ./bin/cake migrations seed --source Seeds/locale/$locale --seed InitDataSeed
 ./bin/cake migrations seed --seed InitDataSeed
-./bin/cake migrations migrate
