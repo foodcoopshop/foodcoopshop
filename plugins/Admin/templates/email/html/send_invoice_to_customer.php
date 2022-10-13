50bdf732a1f5c63e2b6be52ca02d60e31e3fac25
Mario Rothauer <office@rothauer-it.com>
2022-10-13 10:46:07 +0200

    paypal link only if sum > 0

--- plugins/Admin/templates/email/html/send_invoice_to_customer.php
+++ plugins/Admin/templates/email/html/send_invoice_to_customer.php
@@ -31,7 +31,7 @@ use Cake\Core\Configure;
 
             <p><?php echo __d('admin', '{0}_thanks_you_for_your_purchase!', [Configure::read('appDb.FCS_APP_NAME')]); ?></p>
 
-            <?php if (Configure::read('app.paypalMeUsername') != '') { ?>
+            <?php if (Configure::read('app.paypalMeUsername') != '' && $invoiceSumPriceIncl > 0) { ?>
                 <p>
                     <?php echo __d('admin', 'Pay_invoice_with_paypal') . ': https://paypal.me/' . Configure::read('app.paypalMeUsername') . '/' . $invoiceSumPriceIncl . $this->MyHtml->getCurrencyIsoCode(Configure::read('appDb.FCS_CURRENCY_SYMBOL')); ?>
                 </p>
