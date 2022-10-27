b07a5696fef55828c22eba359096f2cfbadcbeb2
mrothauer <office@rothauer-it.com>
2022-10-27 21:47:32 +0200

    fix for extracting strings

--- devtools/update-translations.sh
+++ devtools/update-translations.sh
@@ -5,7 +5,7 @@ source $(dirname $0)/locales.sh
 #get and merge translations for main app
 #to extract core strings change --extract-core to "yes"
 
-bash bin/cake i18n extract --output resources\\locales --paths config,src,templates --overwrite --extract-core no --merge no --no-location --exclude plugins
+bash bin/cake i18n extract --output resources/locales --paths config,src,templates --overwrite --extract-core no --merge no --no-location --exclude plugins
 for locale in "${LOCALES[@]}"
 do
     msgmerge resources/locales/$locale/cake.po resources/locales/cake.pot --output-file=resources/locales/$locale/cake.po --width=1000
