ec48d31d61a08b198bd1d23f7b66c30a3ee9986e
mrothauer <office@rothauer-it.com>
2022-11-02 09:54:10 +0100

    shorter docker container names

--- devtools/init-dev-setup.sh
+++ devtools/init-dev-setup.sh
@@ -3,10 +3,10 @@
 bash ./devtools/installation/set-permissions.sh
 bash ./devtools/setup-dev/copy-config-files.sh
 
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs-php-nginx composer install
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs.php composer install
 
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs-php-nginx bash ./bin/cake migrations migrate -p Queue
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs-php-nginx bash ./bin/cake migrations migrate
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs-php-nginx bash ./bin/cake migrations seed --source Seeds/tests --seed InitTestDataSeed
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs.php bash ./bin/cake migrations migrate -p Queue
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs.php bash ./bin/cake migrations migrate
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /app fcs.php bash ./bin/cake migrations seed --source Seeds/tests --seed InitTestDataSeed
 
-CURRENT_UID=$(id -u):$(id -g) docker exec -w /app/webroot fcs-php-nginx npm install
+CURRENT_UID=$(id -u):$(id -g) docker exec -w /app/webroot fcs.php npm install
