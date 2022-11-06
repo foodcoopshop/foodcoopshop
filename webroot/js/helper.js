ec8ec249fd19d49d6498bcb05027f1c92993a230
mrothauer <office@rothauer-it.com>
2022-11-06 19:51:03 +0100

    remove unused functions

--- webroot/js/helper.js
+++ webroot/js/helper.js
@@ -176,28 +176,6 @@ foodcoopshop.Helper = {
         });
     },
 
-    /**
-     * Returns a function, that, as long as it continues to be invoked, will not
-     * be triggered. The function will be called after it stops being called for
-     * N milliseconds. If `immediate` is passed, trigger the function on the
-     * leading edge, instead of the trailing.
-     * https://davidwalsh.name/javascript-debounce-function
-     */
-    debounce: function(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    },
-
     addPrevAndNextCategoryLinks : function() {
         this.addPrevAndNextLinks(
             '#categories-menu li a',
@@ -323,23 +301,6 @@ foodcoopshop.Helper = {
         return $.grep(array, function(el, index) {
             return index === $.inArray(el, array);
         });
-    },
-
-    getJqueryUiNoButton : function() {
-        return this.getJqueryUiCloseDialogButton(foodcoopshop.LocalizedJs.helper.no);
-    },
-
-    getJqueryUiCancelButton : function() {
-        return this.getJqueryUiCloseDialogButton(foodcoopshop.LocalizedJs.helper.cancel);
-    },
-
-    getJqueryUiCloseDialogButton : function(label) {
-        return {
-            text: label,
-            click: function() {
-                $(this).dialog('close');
-            }
-        };
     },
 
     initBlogPostCarousel: function () {
