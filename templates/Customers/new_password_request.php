201884ac0409e6e28cd9c3fcd10228e1749ae611
mrothauer <office@rothauer-it.com>
2022-11-17 09:50:35 +0100

    light button

--- templates/Customers/new_password_request.php
+++ templates/Customers/new_password_request.php
@@ -36,6 +36,6 @@ $this->element('addScript', ['script' =>
     <p><?php echo __('Please_enter_your_email_address_and_click_on_send_button.'); ?></p>
 
     <?php echo $this->Form->control('Customers.email', ['label' => __('Email')]); ?>
-    <button type="submit" class="btn btn-success"><?php echo __('Send'); ?></button>
+    <button type="submit" class="btn btn-outline-light"><?php echo __('Send'); ?></button>
 
 <?php echo $this->Form->end(); ?>
