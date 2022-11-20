0280cb5af5b43d04d545847309fb4f619b2f76de
mrothauer <office@rothauer-it.com>
2022-11-20 22:25:51 +0100

    add doctype comment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,17 @@
 declare(strict_types=1);
 
 /**
- * Test runner bootstrap.
+ * FoodCoopShop - The open source software for your foodcoop
  *
- * Add additional configuration/setup your application needs when running
- * unit tests in this file.
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 1.0.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
  */
 use Cake\Core\Configure;
 use Cake\Utility\Security;
