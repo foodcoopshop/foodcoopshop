55eab79e559a989a141748c658d1f34f75358266
mrothauer <office@rothauer-it.com>
2023-02-02 09:53:55 +0100

    add csrf token for requests of network module

--- plugins/Network/src/Controller/ApiController.php
+++ plugins/Network/src/Controller/ApiController.php
@@ -70,7 +70,7 @@ class ApiController extends Controller
 
         $this->response = $this->response->withHeader('Access-Control-Allow-Origin', '*');
         $this->response = $this->response->withHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        $this->response = $this->response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $this->response = $this->response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Csrf-Token');
 
         if ($this->getRequest()->is('options')) {
             return $this->getResponse();
