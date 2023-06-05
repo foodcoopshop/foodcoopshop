dd74538d92bea1dfa43818a706b9f87e1cd667d4
mrothauer <office@rothauer-it.com>
2023-06-05 21:37:06 +0200

    color fix

--- plugins/Admin/templates/element/dropdownWithButtons.php
+++ plugins/Admin/templates/element/dropdownWithButtons.php
@@ -27,7 +27,7 @@ $buttons[] = '<a class="dropdown-item" href="' . $helperLink . '" target="_blank
 
 <div class="dropdown">
     <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <i class="fa-solid fa-angles-right"></i> <?php echo $label; ?>
+        <i class="fas fa-angles-right ok"></i> <?php echo $label; ?>
     </button>
     <ul class="dropdown-menu">
         <?php
