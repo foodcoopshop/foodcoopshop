a5767b83b3aa0979f456a8aa5f53776cabdbec4e
mrothauer <office@rothauer-it.com>
2023-10-05 08:37:40 +0200

    remove unused import

--- tests/TestCase/src/Lib/Csv/ProductReaderTest.php
+++ tests/TestCase/src/Lib/Csv/ProductReaderTest.php
@@ -16,7 +16,6 @@ declare(strict_types=1);
  */
 use App\Lib\Csv\ProductReader;
 use App\Test\TestCase\AppCakeTestCase;
-use Cake\Core\Configure;
 
 class ProductReaderTest extends AppCakeTestCase
 {
