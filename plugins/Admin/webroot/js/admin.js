a68508b6001b06546fa275609fe56a2bc3b0930a
mrothauer <office@rothauer-it.com>
2023-11-27 09:54:03 +0100

    fix for row select all

--- plugins/Admin/webroot/js/admin.js
+++ plugins/Admin/webroot/js/admin.js
@@ -101,9 +101,11 @@ foodcoopshop.Admin = {
         var rowMarkerAll = $('input#row-marker-all').on('click', function () {
             var row;
             if (this.checked) {
-                row = $('input.row-marker[type="checkbox"]:not(:checked)');
-                row.prop('checked', true);
-                row.closest('tr').addClass('selected');
+                row = $('input.row-marker[type="checkbox"]:not(:checked):not(:disabled)');
+                if (row.closest('tr').css('display') != 'none') {
+                    row.prop('checked', true);
+                    row.closest('tr').addClass('selected');
+                }
             } else {
                 row = $('input.row-marker[type="checkbox"]:checked');
                 row.prop('checked', false);
