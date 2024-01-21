2180d708daf75c33281bb1327e46af17219f1336
mrothauer <office@rothauer-it.com>
2024-01-21 21:04:09 +0100

    remove hack for correctly setting test database

--- src/Model/Table/AppTable.php
+++ src/Model/Table/AppTable.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace App\Model\Table;
 
 use App\ORM\AppMarshaller;
-use Cake\Datasource\ConnectionManager;
 use Cake\ORM\Marshaller;
 use Cake\ORM\Table;
 
@@ -29,9 +28,6 @@ class AppTable extends Table
     public function initialize(array $config): void
     {
         $this->setTable($this->tablePrefix . $this->getTable());
-        if ((PHP_SAPI == 'cli' && $_SERVER['argv'][0] && preg_match('/phpunit/', $_SERVER['argv'][0]))) {
-            $this->setConnection(ConnectionManager::get('test'));
-        }
         parent::initialize($config);
     }
 
