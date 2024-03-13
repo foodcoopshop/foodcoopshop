9b01029f720f62ede7a3c1ccd5168be82cb33962
mrothauer <office@rothauer-it.com>
2024-03-13 17:18:36 +0100

    AvailableAmountBasedOnUnits Migration

---
+++ config/Migrations/20240313161010_AvailableAmountBasedOnUnits.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AvailableAmountBasedOnUnits extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('fcs_stock_available');
+        $decimalOptions = [
+            'precision' => 10,
+            'scale' => 3,
+        ];
+        $table->changeColumn('quantity', 'decimal', $decimalOptions);
+        $table->changeColumn('quantity_limit', 'decimal', $decimalOptions);
+        $table->changeColumn('sold_out_limit', 'decimal', $decimalOptions);
+        $table->update();
+
+    }
+}
