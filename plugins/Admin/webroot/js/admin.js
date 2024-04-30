d85c58fbf3542d54992c480680fe50585b877332
mrothauer <office@rothauer-it.com>
2024-04-30 15:16:03 +0200

    refactor click listener

--- plugins/Admin/webroot/js/admin.js
+++ plugins/Admin/webroot/js/admin.js
@@ -479,13 +479,19 @@ foodcoopshop.Admin = {
         $('table.list th').css('top', newTop + 11);
     },
 
+    setCheckboxClickCallback : function(callback) {
+        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
+            callback();
+        });
+    },
+
     initGenerateMemberCardsOfSelectedCustomersButton : function() {
         var button = $('#generateMemberCardsOfSelectedCustomersButton');
         foodcoopshop.Helper.disableButton(button);
 
-        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
-            foodcoopshop.Admin.updateObjectSelectionActionButton(button);
-        });
+        foodcoopshop.Admin.setCheckboxClickCallback(() => 
+            foodcoopshop.Admin.updateObjectSelectionActionButton(button)
+        );
 
         button.on('click', function () {
             var customerIds = foodcoopshop.Admin.getSelectedIds();
@@ -497,9 +503,9 @@ foodcoopshop.Admin = {
         var button = $('#generateProductCardsOfSelectedProductsButton');
         foodcoopshop.Helper.disableButton(button);
 
-        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
-            foodcoopshop.Admin.updateObjectSelectionActionButton(button);
-        });
+        foodcoopshop.Admin.setCheckboxClickCallback(() => 
+            foodcoopshop.Admin.updateObjectSelectionActionButton(button)
+        );
 
         button.on('click', function () {
             var productIds = foodcoopshop.Admin.getSelectedProductIds();
@@ -511,9 +517,9 @@ foodcoopshop.Admin = {
         var button = $('#exportProductsButton');
         foodcoopshop.Helper.disableButton(button);
 
-        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
-            foodcoopshop.Admin.updateObjectSelectionActionButton(button);
-        });
+        foodcoopshop.Admin.setCheckboxClickCallback(() => 
+            foodcoopshop.Admin.updateObjectSelectionActionButton(button)
+        );
 
         button.on('click', function () {
             var productIds = foodcoopshop.Admin.getSelectedProductIds();
@@ -525,9 +531,9 @@ foodcoopshop.Admin = {
         var button = $('#exportCustomersButton');
         foodcoopshop.Helper.disableButton(button);
 
-        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
-            foodcoopshop.Admin.updateObjectSelectionActionButton(button);
-        });
+        foodcoopshop.Admin.setCheckboxClickCallback(() => 
+            foodcoopshop.Admin.updateObjectSelectionActionButton(button)
+        );
 
         button.on('click', function () {
             var customerIds = foodcoopshop.Admin.getSelectedIds();
