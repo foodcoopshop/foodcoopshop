82ae0944fb0c63f38f7dbf770bafb13fe26ae86b
mrothauer <office@rothauer-it.com>
2024-07-01 12:17:50 +0200

    init ru_RU

---
+++ templates/element/legal/ru_RU/retail/termsOfUse.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 4.1.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+
+?>
+<h1>Terms of use</h1>
+
+<h2>Platform owner</h2>
+
+<p><?php echo $this->Html->getPlatformOwnerForLegalTexts(); ?></p>
