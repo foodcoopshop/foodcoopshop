d7f9aefaaf93435fc9251b07b2b55405ce68b973
mrothauer <office@rothauer-it.com>
2024-07-06 22:39:25 +0200

    fix for new tcpdf font (tests)

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -3,8 +3,9 @@
 # remove overhead from tcpdf library
 rm -Rf vendor/tecnickcom/tcpdf/examples
 
-#delete all files and folders in folder /fonts except starting with freesans*
-find vendor/tecnickcom/tcpdf/fonts/ ! -name freesans* -type f -delete
+#delete all files and folders in folder /fonts except starting with freesans* or helvetica*
+find vendor/tecnickcom/tcpdf/fonts/ -maxdepth 1 -type f ! -name 'helvetica*' ! -name 'freesans*' -delete
+
 
 rm -Rf vendor/studio-42/elfinder/.git
 mkdir -p webroot/js/elfinder
