6c70b71742a9d41ea6e5977d04c47aeaf77b0f51
mrothauer <office@rothauer-it.com>
2024-07-07 18:32:56 +0200

    fix

--- devtools/composer-post-install.sh
+++ devtools/composer-post-install.sh
@@ -4,8 +4,7 @@
 rm -Rf vendor/tecnickcom/tcpdf/examples
 
 #delete all files and folders in folder /fonts except starting with freesans* or helvetica*
-find vendor/tecnickcom/tcpdf/fonts/ -maxdepth 1 -type f ! -name 'helvetica*' ! -name 'freesans*' -delete
-
+find vendor/tecnickcom/tcpdf/fonts/ ! -name 'helvetica*' ! -name 'freesans*' -delete
 
 rm -Rf vendor/studio-42/elfinder/.git
 mkdir -p webroot/js/elfinder
