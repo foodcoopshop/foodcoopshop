cd2cc9317471e260b9e71d54c6bcc19f0ebbe99a
mrothauer <office@rothauer-it.com>
2024-08-07 19:53:02 +0200

    return early and often

--- src/Model/Table/CartsTable.php
+++ src/Model/Table/CartsTable.php
@@ -53,19 +53,24 @@ class CartsTable extends AppTable
 
     public function validationDefault(Validator $validator): Validator
     {
-        $orderCustomerService = new OrderCustomerService();
-        if (!(Configure::read('app.selfServiceShowConfirmDialogOnSubmit') && $orderCustomerService->isSelfServiceMode())){
-            if (Configure::read('app.rightOfWithdrawalEnabled')) {
-                $validator->requirePresence('cancellation_terms_accepted', true, __('Please_accept_the_information_about_right_of_withdrawal'));
-                $validator->equals('cancellation_terms_accepted', 1, __('Please_accept_the_information_about_right_of_withdrawal.'));
-            }
-            if (Configure::read('app.generalTermsAndConditionsEnabled')) {
-                $validator->requirePresence('general_terms_and_conditions_accepted', true, __('Please_accept_the_general_terms_and_conditions.'));
-                $validator->equals('general_terms_and_conditions_accepted', 1, __('Please_accept_the_general_terms_and_conditions.'));
-            }
-        }
         $validator->notEmptyArray('self_service_payment_type', __('Please_select_your_payment_type.'));
+
+        if (Configure::read('app.selfServiceShowConfirmDialogOnSubmit') && (new OrderCustomerService())->isSelfServiceMode()) {
+            return $validator;
+        }
+
+        if (Configure::read('app.rightOfWithdrawalEnabled')) {
+            $validator->requirePresence('cancellation_terms_accepted', true, __('Please_accept_the_information_about_right_of_withdrawal'));
+            $validator->equals('cancellation_terms_accepted', 1, __('Please_accept_the_information_about_right_of_withdrawal.'));
+        }
+
+        if (Configure::read('app.generalTermsAndConditionsEnabled')) {
+            $validator->requirePresence('general_terms_and_conditions_accepted', true, __('Please_accept_the_general_terms_and_conditions.'));
+            $validator->equals('general_terms_and_conditions_accepted', 1, __('Please_accept_the_general_terms_and_conditions.'));
+        }
+
         return $validator;
+
     }
 
     /**
