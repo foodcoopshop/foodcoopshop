61ffe73c219eff538ee8d63fababacf37e6d8f48
pabneukistl Tanja <info@pabneukistl.at>
2024-09-21 23:07:32 +0000

    totalProductCount

--- src/Controller/SelfServiceController.php
+++ src/Controller/SelfServiceController.php
@@ -104,7 +104,7 @@ class SelfServiceController extends FrontendController
             $categoryIdForSearch = Configure::read('app.categoryAllProducts');
         }
         $products = $catalogService->getProducts($categoryIdForSearch, false, $keyword, 0, false, Configure::read('app.selfServiceModeShowOnlyStockProducts'), page: $page);
-        $totalProductCount = $totalProductCount = $catalogService->getProducts($categoryIdForSearch, false, $keyword, 0, true, Configure::read('app.selfServiceModeShowOnlyStockProducts'));
+        $totalProductCount = $catalogService->getProducts($categoryIdForSearch, false, $keyword, 0, true, Configure::read('app.selfServiceModeShowOnlyStockProducts'));
         $pagesCount = $catalogService->getPagesCount($totalProductCount);
         $products = $catalogService->prepareProducts($products);
 
