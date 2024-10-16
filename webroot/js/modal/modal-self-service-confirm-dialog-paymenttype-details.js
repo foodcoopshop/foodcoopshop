ce20cefb86e9a6e31076e27270e4393bc4cad070
pabneukistl Tanja <info@pabneukistl.at>
2024-10-16 15:35:21 +0000

    set value for modal again is modal is opened again, otherwise old value will be showed

--- webroot/js/modal/modal-self-service-confirm-dialog-paymenttype-details.js
+++ webroot/js/modal/modal-self-service-confirm-dialog-paymenttype-details.js
@@ -20,10 +20,6 @@ foodcoopshop.ModalSelfServicePaymenttypeDetailsDialog = {
 
     getOpenHandler : function(modalSelector, paymentName, paymentText) {
         var modalSelector = '#self-service-confirm-dialog-paymenttype-details';
-        var title = paymentName;
-        var amount = $('p.total-sum-wrapper > span.sum').html();
-        var html = '<p>' + foodcoopshop.LocalizedJs.cart.selfServiceAmountToBePaid + amount + paymentText+ '</p>';
-
         var buttons = [
             foodcoopshop.Modal.createButton(['btn-success'], foodcoopshop.LocalizedJs.cart.selfServiceConfirmPurchaseButton, 'fa-fw fas fa-check'),
             foodcoopshop.Modal.createButton(['btn-outline-light'], foodcoopshop.LocalizedJs.cart.selfServiceDenyPurchaseButton, null, true)
@@ -31,10 +27,14 @@ foodcoopshop.ModalSelfServicePaymenttypeDetailsDialog = {
 
         foodcoopshop.Modal.appendModalToDom(
             modalSelector,
-            title,
-            html,
+            '',
+            '',
             buttons
         );
+
+        var amount = $('p.total-sum-wrapper > span.sum').html();
+        $(modalSelector + ' .modal-title').text(paymentName);
+        $(modalSelector + ' .modal-body').html('<p>' + foodcoopshop.LocalizedJs.cart.selfServiceAmountToBePaid + amount + paymentText + '</p>');
 
         foodcoopshop.Modal.bindSuccessButton(modalSelector, function() {
             foodcoopshop.ModalSelfServicePaymenttypeDetailsDialog.getSuccessHandler();
