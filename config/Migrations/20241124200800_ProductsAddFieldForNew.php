03bd4774b8a1e292a45a0cb5a85d5943f0b46ff0
mrothauer <office@rothauer-it.com>
2024-11-26 14:08:51 +0100

    restore product.created from actionlogs

--- config/Migrations/20241124200800_ProductsAddFieldForNew.php
+++ config/Migrations/20241124200800_ProductsAddFieldForNew.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Datasource\FactoryLocator;
 use Migrations\AbstractMigration;
 
 class ProductsAddFieldForNew extends AbstractMigration
@@ -13,5 +14,22 @@ class ProductsAddFieldForNew extends AbstractMigration
             'null' => true,
         ])->update();
         $this->execute('UPDATE fcs_product SET new = created');
+
+        $actionLogsTable = FactoryLocator::get('Table')->get('ActionLogs');
+        $actionLogs = $actionLogsTable->find()
+            ->where([
+                $actionLogsTable->aliasField('type') => 'product_added',
+            ]);
+
+        $productTable = FactoryLocator::get('Table')->get('Products');
+        foreach ($actionLogs as $actionLog) {
+            $productEntity = $productTable->find()->where([$productTable->aliasField('id_product') => $actionLog->object_id])->first();
+            if (empty($productEntity)) {
+                continue;
+            }
+            $productEntity->created = $actionLog->date;
+            $productTable->save($productEntity, ['validate' => false]);
+        }
+
     }
 }
