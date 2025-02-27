6e7c22c5f088462b15ee9fb2aa497339197c0bbe
mrothauer <office@rothauer-it.com>
2025-02-27 10:04:15 +0100

    cleanup and simplify test

--- src/Model/Traits/CartValidatorTrait.php
+++ src/Model/Traits/CartValidatorTrait.php
@@ -26,34 +26,40 @@ trait CartValidatorTrait
 
     public function isAmountAvailableAttribute($isStockProduct, $stockManagementEnabled, $alwaysAvailable, $availableQuantity, $amount, $attributeName, $productName, $unitName = ''): bool|string
     {
-        $result = true;
-        if (Configure::read('app.selfServiceIsAmountValidationEnabled') || !((new OrderCustomerService())->isSelfServiceMode())){
-            $unitNameString = ($unitName != '') ? ' ' . $unitName : '';
-            if ((($isStockProduct && $stockManagementEnabled) || !$alwaysAvailable) && $availableQuantity < $amount) {
-                $result = __('The_desired_amount_{0}_of_the_attribute_{1}_of_the_product_{2}_is_not_available_any_more_available_amount_{3}.', [
-                    '<b>' . Configure::read('app.numberHelper')->formatUnitAsDecimal($amount) . $unitNameString . '</b>',
-                    '<b>' . $attributeName . '</b>',
-                    '<b>' . $productName . '</b>',
-                    Configure::read('app.numberHelper')->formatUnitAsDecimal($availableQuantity) . $unitNameString,
-                ]);
-            }
+        if (!Configure::read('app.selfServiceIsAmountValidationEnabled') && (new OrderCustomerService())->isSelfServiceMode()) {
+            return true;
         }
+        
+        $result = true;
+        $unitNameString = ($unitName != '') ? ' ' . $unitName : '';
+        if ((($isStockProduct && $stockManagementEnabled) || !$alwaysAvailable) && $availableQuantity < $amount) {
+            $result = __('The_desired_amount_{0}_of_the_attribute_{1}_of_the_product_{2}_is_not_available_any_more_available_amount_{3}.', [
+                '<b>' . Configure::read('app.numberHelper')->formatUnitAsDecimal($amount) . $unitNameString . '</b>',
+                '<b>' . $attributeName . '</b>',
+                '<b>' . $productName . '</b>',
+                Configure::read('app.numberHelper')->formatUnitAsDecimal($availableQuantity) . $unitNameString,
+            ]);
+        }
+    
         return $result;
     }
 
     public function isAmountAvailableProduct($isStockProduct, $stockManagementEnabled, $alwaysAvailable, $attributeId, $availableQuantity, $amount, $productName, $unitName = ''): bool|string
     {
-        $result = true;
-        if (Configure::read('app.selfServiceIsAmountValidationEnabled') || !((new OrderCustomerService())->isSelfServiceMode())){
-            $unitNameString = ($unitName != '') ? ' ' . $unitName : '';
-            if ((($isStockProduct && $stockManagementEnabled) || !$alwaysAvailable) && $attributeId == 0 && $availableQuantity < $amount) {
-                $result = __('The_desired_amount_{0}_of_the_product_{1}_is_not_available_any_more_available_amount_{2}.', [
-                    '<b>' . Configure::read('app.numberHelper')->formatUnitAsDecimal($amount) . $unitNameString . '</b>',
-                    '<b>' . $productName . '</b>',
-                    Configure::read('app.numberHelper')->formatUnitAsDecimal($availableQuantity) . $unitNameString,
-                ]);
-            }
+        if (!Configure::read('app.selfServiceIsAmountValidationEnabled') && (new OrderCustomerService())->isSelfServiceMode()) {
+            return true;
         }
+        
+        $result = true;
+        $unitNameString = ($unitName != '') ? ' ' . $unitName : '';
+        if ((($isStockProduct && $stockManagementEnabled) || !$alwaysAvailable) && $attributeId == 0 && $availableQuantity < $amount) {
+            $result = __('The_desired_amount_{0}_of_the_product_{1}_is_not_available_any_more_available_amount_{2}.', [
+                '<b>' . Configure::read('app.numberHelper')->formatUnitAsDecimal($amount) . $unitNameString . '</b>',
+                '<b>' . $productName . '</b>',
+                Configure::read('app.numberHelper')->formatUnitAsDecimal($availableQuantity) . $unitNameString,
+            ]);
+        }
+        
         return $result;
     }
 
