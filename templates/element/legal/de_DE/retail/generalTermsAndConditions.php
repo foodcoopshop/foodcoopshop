3d86fb24378f7a9751f476a16c140a6eb01a4daa
HatlaufMartin <martin.hatlauf@htl-wels.at>
2025-07-08 09:17:23 +0200

    Changed legal texts now respect member fee

--- templates/element/legal/de_DE/retail/generalTermsAndConditions.php
+++ templates/element/legal/de_DE/retail/generalTermsAndConditions.php
@@ -31,7 +31,7 @@ use Cake\Core\Configure;
 
 <h2>2. Leistungen und Entgelte</h2>
 
-<p>2.1. Der Betreiber stellt dem Nutzer eine Plattform unentgeltlich zur Verfügung, auf der Waren und Dienstleistungen Dritter präsentiert werden. Diese dargebotenen Waren und Dienstleistungen sind eine unverbindliche Aufforderung an den Nutzer, ein verbindliches Anbot für die angebotenen Waren und Dienstleistungen zu legen. Ein verbindliches Angebot durch den Nutzer kann nur gelegt werden, wenn dieser sich auf der Plattform registriert hat und zustimmt, dass seine personenbezogenen Daten für die Durchführung von Bestellungen verwendet werden. Durch eine getätigte Bestellung legt der Nutzer ein solches verbindliches Anbot an den Betreiber. Ein Vertrag zwischen dem Nutzer und dem Betreiber kommt dann zustande, wenn der Drittanbieter mit der Leistungserbringung an den Betreiber begonnen hat oder die Waren zur Abholung bereitgelegt hat.</b></p>
+<p>2.1. Der Betreiber stellt dem Nutzer eine Plattform zur Verfügung, auf der Waren und Dienstleistungen Dritter präsentiert werden. Diese dargebotenen Waren und Dienstleistungen sind eine unverbindliche Aufforderung an den Nutzer, ein verbindliches Anbot für die angebotenen Waren und Dienstleistungen zu legen. Ein verbindliches Angebot durch den Nutzer kann nur gelegt werden, wenn dieser sich auf der Plattform registriert hat und zustimmt, dass seine personenbezogenen Daten für die Durchführung von Bestellungen verwendet werden. Durch eine getätigte Bestellung legt der Nutzer ein solches verbindliches Anbot an den Betreiber. Ein Vertrag zwischen dem Nutzer und dem Betreiber kommt dann zustande, wenn der Drittanbieter mit der Leistungserbringung an den Betreiber begonnen hat oder die Waren zur Abholung bereitgelegt hat.</b></p>
 
 <p>2.2. Der Vertrag über die Waren und Dienstleistungen kommt zwischen dem Nutzer und dem Betreiber zustande.</p>
 
@@ -41,7 +41,7 @@ use Cake\Core\Configure;
 
 <h2>3. Schadenersatz und Gewährleistung</h2>
 
-<p>3.1. Die Nutzung der Plattform ist für die Nutzer kostenlos. Eine Haftung ist daher ausgeschlossen.</p>
+<p>3.1. Die Nutzung der Plattform ist für die Nutzer prinzipiell kostenlos, eine Haftung ist daher ausgeschlossen. Es steht dem Betreiber der Plattform allerdings frei, für die Nutzung einen Mitgliedsbeitrag zu einzuheben.</p>
 
 <p>3.2. Für Schäden infolge schuldhafter Vertragsverletzung haftet der Betreiber bei eigenem Verschulden oder dem eines Erfüllungsgehilfen nur für Vorsatz oder grobe Fahrlässigkeit. Dies gilt nicht für Schäden an der Person.</p>
 
