cfa7437ffb45f3103e2d54a2b005cf7a98332caa
HatlaufMartin <martin.hatlauf@htl-wels.at>
2025-07-11 11:53:20 +0200

    Button added for copying one Product with dropdown to select amount

--- plugins/Admin/src/Traits/Products/DuplicateTrait.php
+++ plugins/Admin/src/Traits/Products/DuplicateTrait.php
@@ -39,93 +39,93 @@ trait DuplicateTrait
             ]
         );
 
-        // copy the data
-        // need to check if $product-id is in units table
-        // if yes put all the new id's there with the correct values.
-        for ($i = 0; $i < $copyOptions['amount']; $i++) {
-            $productsTable->newEntity([
-                'name' =>
-                    __d('admin', 'Copy ({0}) of {1}', [
-                        $i,
-                        $product->getName(),
-                    ]),
-                'price' =>
-                    $product->getPrice(),
-                'new' =>
-                    $product->getNew(),
-
-
-            ]);
-        }
-
-        $preparedProductForActionLog = [];
-        $preparedProductForActionLog[] = '<b>' . $product->name . '</b>: ID ' . $product->id_product . ',  ' . $product->manufacturer->name;
-
-        $cronjobsTable = $this->getTableLocator()->get('Cronjobs');
-        if ($cronjobsTable->isInvoiceCronjobActive()) {
-            try {
-                // check if open order exist
-                $orderDetailsTable = $this->getTableLocator()->get('OrderDetails');
-                $query = $orderDetailsTable->find('all',
-                    conditions: [
-                        $orderDetailsTable->aliasField('product_id') => $productId,
-                        $orderDetailsTable->aliasField('order_state IN') => [
-                            OrderDetail::STATE_OPEN,
-                            OrderDetail::STATE_ORDER_LIST_SENT_TO_MANUFACTURER,
-                        ],
-                    ],
-                    contain: [
-                        'Products',
-                    ]
-                );
-                $query->select(
-                    [
-                        'orderDetailsCount' => $query->func()->count('OrderDetails.product_id'),
-                        'productName' => 'Products.name'
-                    ]
-                );
-                $query->groupBy('OrderDetails.product_id');
-
-                $errors = [];
-                if ($query->count() > 0) {
-                    foreach ($query as $orderDetail) {
-                        $errors[] = __d('admin', 'The_product_{0}_has_{1,plural,=1{1_open_order} other{#_open_orders}}.',
-                            [
-                                '<b>' . $orderDetail->productName . '</b>',
-                                $orderDetail->orderDetailsCount,
-                            ]
-                        );
-                    }
-                }
-                if (!empty($errors)) {
-                    $errorString = '<ul><li>' . join('</li><li>', $errors) . '</li></ul>';
-                    $errorString .= __d('admin', 'Please_try_again_as_soon_as_the_next_invoice_has_been_generated.');
-                    throw new \Exception($errorString);
-                }
-            } catch (\Exception $e) {
-                return $this->sendAjaxError($e);
-            }
-        }
-
-        // 1) set field active to -1
-        $productsTable->updateAll([
-            'active' => APP_DEL,
-            'modified' => DateTime::now() // timestamp behavior does not work here...
-        ], [
-            'id_product IN' => $productId
-        ]);
-
-        $message = __d('admin', 'product_was_copied_successfully.');
-
-        $this->Flash->success($message);
-        $actionLogsTable = $this->getTableLocator()->get('ActionLogs');
-        $actionLogsTable->customSave('product_deleted', $this->identity->getId(), 0, 'products', $message . '<br />' . join('<br />', $preparedProductForActionLog));
-
-        $this->set([
-            'status' => 1,
-            'msg' => 'ok',
-        ]);
-        $this->viewBuilder()->setOption('serialize', ['status', 'msg']);
+//        // copy the data
+//        // need to check if $product-id is in units table
+//        // if yes put all the new id's there with the correct values.
+//        for ($i = 0; $i < $copyOptions['amount']; $i++) {
+//            $productsTable->newEntity([
+//                'name' =>
+//                    __d('admin', 'Copy ({0}) of {1}', [
+//                        $i,
+//                        $product->getName(),
+//                    ]),
+//                'price' =>
+//                    $product->getPrice(),
+//                'new' =>
+//                    $product->getNew(),
+//
+//
+//            ]);
+//        }
+//
+//        $preparedProductForActionLog = [];
+//        $preparedProductForActionLog[] = '<b>' . $product->name . '</b>: ID ' . $product->id_product . ',  ' . $product->manufacturer->name;
+//
+//        $cronjobsTable = $this->getTableLocator()->get('Cronjobs');
+//        if ($cronjobsTable->isInvoiceCronjobActive()) {
+//            try {
+//                // check if open order exist
+//                $orderDetailsTable = $this->getTableLocator()->get('OrderDetails');
+//                $query = $orderDetailsTable->find('all',
+//                    conditions: [
+//                        $orderDetailsTable->aliasField('product_id') => $productId,
+//                        $orderDetailsTable->aliasField('order_state IN') => [
+//                            OrderDetail::STATE_OPEN,
+//                            OrderDetail::STATE_ORDER_LIST_SENT_TO_MANUFACTURER,
+//                        ],
+//                    ],
+//                    contain: [
+//                        'Products',
+//                    ]
+//                );
+//                $query->select(
+//                    [
+//                        'orderDetailsCount' => $query->func()->count('OrderDetails.product_id'),
+//                        'productName' => 'Products.name'
+//                    ]
+//                );
+//                $query->groupBy('OrderDetails.product_id');
+//
+//                $errors = [];
+//                if ($query->count() > 0) {
+//                    foreach ($query as $orderDetail) {
+//                        $errors[] = __d('admin', 'The_product_{0}_has_{1,plural,=1{1_open_order} other{#_open_orders}}.',
+//                            [
+//                                '<b>' . $orderDetail->productName . '</b>',
+//                                $orderDetail->orderDetailsCount,
+//                            ]
+//                        );
+//                    }
+//                }
+//                if (!empty($errors)) {
+//                    $errorString = '<ul><li>' . join('</li><li>', $errors) . '</li></ul>';
+//                    $errorString .= __d('admin', 'Please_try_again_as_soon_as_the_next_invoice_has_been_generated.');
+//                    throw new \Exception($errorString);
+//                }
+//            } catch (\Exception $e) {
+//                return $this->sendAjaxError($e);
+//            }
+//        }
+//
+//        // 1) set field active to -1
+//        $productsTable->updateAll([
+//            'active' => APP_DEL,
+//            'modified' => DateTime::now() // timestamp behavior does not work here...
+//        ], [
+//            'id_product IN' => $productId
+//        ]);
+//
+//        $message = __d('admin', 'product_was_copied_successfully.');
+//
+//        $this->Flash->success($message);
+//        $actionLogsTable = $this->getTableLocator()->get('ActionLogs');
+//        $actionLogsTable->customSave('product_deleted', $this->identity->getId(), 0, 'products', $message . '<br />' . join('<br />', $preparedProductForActionLog));
+//
+//        $this->set([
+//            'status' => 1,
+//            'msg' => 'ok',
+//        ]);
+//        $this->viewBuilder()->setOption('serialize', ['status', 'msg']);
         return null;
 
     }
---
+++ plugins/Admin/webroot/js/modal/modal-product-duplicate.js
@@ -0,0 +1,108 @@
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 4.2.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Martin Hatlauf <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+foodcoopshop.ModalProductDuplicate = {
+
+    init : function() {
+
+        var modalSelector = '#modal-product-duplicate';
+
+        var button = $('#duplicateSelectedProduct');
+        foodcoopshop.Helper.disableButton(button);
+
+        $('table.list').find('input.row-marker[type="checkbox"],#row-marker-all').on('click', function () {
+            foodcoopshop.Helper.disableButton(button);
+            if ($('table.list').find('input.row-marker[type="checkbox"]:checked').length === 1) {
+                foodcoopshop.Helper.enableButton(button);
+            }
+        });
+
+        button.on('click', function () {
+
+            var productIds = foodcoopshop.Admin.getSelectedProductIds();
+            var title = foodcoopshop.LocalizedJs.admin.CopyProduct;
+
+            var html = '<p style="margin-top: 10px;">';
+            html += foodcoopshop.LocalizedJs.admin.ReallyCopyProduct;
+            html += '</p>';
+
+            var product = $('tr#product-' + productIds[0] + ' span.product-name').html();
+
+            html += '<ul><li>' + product + '</li></ul>';
+
+            html += '<div class="field-wrapper">';
+            html += '<label class="dynamic-element default" for="copy-amount">'+ foodcoopshop.LocalizedJs.admin.AmountOfCopies +'</label><br>';
+            html += '<select id="copy-amount" name="copy-amount" style="margin-top: 5px;">';
+            for (var i = 1; i <= 10; i++) {
+                html += '<option value= "'+i+'" >' + i + '</option>';
+            }
+            html += '</select>';
+            html += '</div>';
+
+            foodcoopshop.Modal.appendModalToDom(
+                modalSelector,
+                title,
+                html
+            );
+
+            foodcoopshop.Modal.bindSuccessButton(modalSelector, function() {
+                var amountValue = parseInt($(modalSelector + ' #copy-amount').val());
+                foodcoopshop.ModalProductDuplicate.getSuccessHandler(modalSelector, productIds, amountValue);
+            });
+
+            $(modalSelector).on('hidden.bs.modal', function (e) {
+                foodcoopshop.ModalProductDuplicate.getCloseHandler(modalSelector);
+            });
+            foodcoopshop.ModalProductDuplicate.getOpenHandler(modalSelector);
+
+        });
+
+    },
+
+    getCloseHandler : function(modalSelector) {
+        $(modalSelector).remove();
+    },
+
+    getSuccessHandler : function(modalSelector, productIds, amount) {
+        var copyOptions = {};
+        copyOptions['amount'] = amount;
+
+        foodcoopshop.Helper.ajaxCall(
+            '/admin/products/duplicate/',
+            {
+                productIds: productIds,
+                copyOptions: copyOptions
+            },
+            {
+                onOk: function (data) {
+                    document.location.reload();
+                },
+                onError: function (data) {
+                    var message = '<p>';
+
+                    message += foodcoopshop.LocalizedJs.admin.ErrorsOccurredWhileProductWasCopied;
+
+                    message += ':</p>';
+                    message = message + data.msg;
+                    foodcoopshop.Modal.appendFlashMessageError(modalSelector, message);
+                    foodcoopshop.Modal.resetButtons(modalSelector);
+                }
+            }
+        );
+    },
+
+    getOpenHandler : function(modalSelector) {
+        new bootstrap.Modal(document.getElementById(modalSelector.replace(/#/, ''))).show();
+    }
+
+};
