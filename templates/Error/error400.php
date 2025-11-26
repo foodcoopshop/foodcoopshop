dbc58c7dde7058ee6bee2ba50e66107edebdd8b8
mrothauer <office@rothauer-it.com>
2025-11-26 17:24:58 +0100

    resolve debugStaticCallUse

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -8,42 +8,11 @@ declare(strict_types=1);
  * For full copyright and license information, please see LICENSE
  * Redistributions of files must retain the above copyright notice.
  *
- * @since         FoodCoopShop 1.0.0
+ * @since         FoodCoopShop 4.2.0
  * @license       https://opensource.org/licenses/AGPL-3.0
  * @author        Mario Rothauer <office@foodcoopshop.com>
  * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
  * @link          https://www.foodcoopshop.com
  */
 
-use Cake\Core\Configure;
-use Cake\Error\Debugger;
-
-if (Configure::read('debug')) :
-    $this->layout = 'dev_error';
-    $this->assign('title', $message);
-    $this->assign('templateName', 'error400.ctp');
-
-    $this->start('file');
-?>
-<?php if (!empty($error->queryString)) : ?>
-    <p class="notice">
-        <strong>SQL Query: </strong>
-        <?= h($error->queryString) ?>
-    </p>
-<?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-        <strong>SQL Query Params: </strong>
-        <?php Debugger::dump($error->params) ?>
-<?php endif; ?>
-<?= $this->element('auto_table_warning') ?>
-<?php
-$this->end();
-endif;
-
-// START production error page
-if (!Configure::read('debug')) {
-    $this->layout = 'error';
-    echo '<a href="/"><img id="installation-logo" src="/files/images/' . Configure::read('app.logoFileName') . '" /></a>';
-    echo '<h2>'.$message.'</h2>';
-}
-?>
+echo $this->element('error');
--- templates/element/error.php
+++ templates/element/error.php
@@ -8,11 +8,23 @@ declare(strict_types=1);
  * For full copyright and license information, please see LICENSE
  * Redistributions of files must retain the above copyright notice.
  *
- * @since         FoodCoopShop 1.0.0
+ * @since         FoodCoopShop 4.2.0
  * @license       https://opensource.org/licenses/AGPL-3.0
  * @author        Mario Rothauer <office@foodcoopshop.com>
  * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
  * @link          https://www.foodcoopshop.com
  */
 
-echo $this->element('error');
+use Cake\Core\Configure;
+
+if (Configure::read('debug')) {
+    $this->layout = 'dev_error';
+    $this->assign('title', $message);
+    $this->start('file');
+    $this->end();
+} else {
+    $this->layout = 'error';
+    echo '<a href="/"><img id="installation-logo" src="/files/images/' . Configure::read('app.logoFileName') . '" /></a>';
+    echo '<h2>'.$message.'</h2>';
+}
+?>
