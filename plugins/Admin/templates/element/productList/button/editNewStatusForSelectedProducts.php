2e9adbf0c9fac4bff42b2e0699d3dd4d94df1b4c
Mario Rothauer <office@rothauer-it.com>
2025-12-03 15:19:48 +0100

    Issue 1215 bulk update product new status (#1219)
    * add composer phpstan script
    
    * first  try bulk edit new status
    
    * finetuning
    
    * wording
    
    * add test
    
    * fixes

---
+++ plugins/Admin/templates/element/productList/button/editNewStatusForSelectedProducts.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 4.2.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+
+if (empty($products)) {
+    return false;
+}
+
+echo '<a id="editNewStatusForSelectedProducts" class="dropdown-item" href="javascript:void(0);"><i class="fa-fw fas fa-star"></i> ' . __d('admin', 'Edit_new_status') . '</a>';
