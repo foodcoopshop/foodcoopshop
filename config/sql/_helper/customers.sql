d8e549fc2f47083b9bdc08d9be8001c631029c32
mrothauer <office@rothauer-it.com>
2026-02-27 09:20:54 +0100

    cleanup

--- config/sql/_helper/customers.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.id_customer, c.firstname, c.lastname, c.email, c.date_add, a.address1, a.address2, a.postcode, a.city, a.phone, a.phone_mobile
-FROM fcs_customer c
-LEFT join fcs_address a ON a.id_customer = c.id_customer
-WHERE a.id_manufacturer = 0
-ORDER BY c.lastname
-LIMIT 10000
