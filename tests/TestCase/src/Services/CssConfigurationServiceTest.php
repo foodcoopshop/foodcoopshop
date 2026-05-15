11d9c9dac4f2832124fdef9da8146707805fd83b
mrothauer <office@rothauer-it.com>
2026-05-15 09:40:57 +0200

    update

--- tests/TestCase/src/Services/CssConfigurationServiceTest.php
+++ tests/TestCase/src/Services/CssConfigurationServiceTest.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 use App\Services\CssConfigurationService;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 4.3.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
 class CssConfigurationServiceTest extends TestCase
 {
 
