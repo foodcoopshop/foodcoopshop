dd144498ff8f6857d81bae316fec91dcd8b0ff65
mrothauer <office@rothauer-it.com>
2026-07-01 13:31:09 +0200

    add 4.9 tax

--- config/Seeds/AddTaxesAustriaSeed.php
+++ config/Seeds/AddTaxesAustriaSeed.php
@@ -11,7 +11,8 @@ class AddTaxesAustriaSeed extends BaseSeed
             INSERT INTO `fcs_tax` VALUES
             (1,20.000,1,0),
             (2,10.000,1,0),
-            (3,13.000,1,0);
+            (3,13.000,1,0),
+            (4,4.900,1,0);
         ";
         $this->execute($query);
     }
